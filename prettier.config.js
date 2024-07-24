522559b18a10818bab50ade09a7cb18644f64252
Brian Baugher <123590980+brian-baugher@users.noreply.github.com>
2024-07-24 17:32:51 -0400

    chore: Spin up dependencies (#1)
    Get everything ready for development (scripts, packages, directories,
    etc)

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: "es5",
+};
+
+export default config;
---
+++ src/components/myComponent.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export const MyComponent = () => {
+  return (
+    <>
+      <h1>hello world</h1>
+    </>
+  );
+};
