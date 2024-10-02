bf17250b08075c1cb210c6c2fad9cf881d0e7350
Brian Stephan <bstephan@yext.com>
2024-10-02 13:17:09 -0400

    feat(util): add themeResolver

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { renderEntityFields } from "./renderEntityFields.tsx";
 export { resolveVisualEditorData } from "./resolveVisualEditorData.ts";
 export { resolveYextEntityField } from "./resolveYextEntityField.ts";
+export { themeResolver } from "./themeResolver.ts";
