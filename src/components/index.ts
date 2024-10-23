a225c2a1db0315dd87cc480d9ec7080a38adc7dd
Jacob Wartofsky <jwartofsky@yext.com>
2024-10-23 13:19:16 -0400

    feat: add Image component
    Adds an image component which can be used in the pages starter
    repo.
    
    Note that to use this component, all instances of useDocument
    must be replaces with the VE version.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { Editor } from "./Editor.tsx";
 export { EntityField } from "./EntityField.tsx";
 export { YextEntityFieldSelector } from "./YextEntityFieldSelector.tsx";
+export { ImageWrapperComponent, type ImageWrapperProps } from "./Image.tsx";
 export type {
   RenderYextEntityFieldSelectorProps,
   YextEntityField,
