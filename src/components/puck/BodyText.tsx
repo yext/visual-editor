20935966abdd08d8466ef2e5d966d9fcf01d7f8d
briantstephan <70166284+briantstephan@users.noreply.github.com>
2024-10-24 17:00:26 -0400

    feat: add body text component (#108)
    Tested with a local VE build in dev mode and the component worked as
    expected.

---
+++ src/components/puck/BodyText.tsx
@@ -0,0 +1,100 @@
+import * as React from "react";
+import { ComponentConfig, Fields } from "@measured/puck";
+import { Body, BodyProps, bodyVariants } from "./atoms/body.js";
+import { useDocument } from "../../hooks/useDocument.js";
+import {
+  YextEntityField,
+  YextEntityFieldSelector,
+} from "../editor/YextEntityFieldSelector.js";
+import { EntityField } from "../editor/EntityField.js";
+import { resolveYextEntityField } from "../../utils/resolveYextEntityField.js";
+
+export interface BodyTextProps extends BodyProps {
+  text: YextEntityField;
+}
+
+const BodyText = React.forwardRef<HTMLParagraphElement, BodyTextProps>(
+  ({ text, ...bodyProps }, ref) => {
+    const document = useDocument();
+
+    return (
+      <EntityField fieldId={text.field}>
+        <Body ref={ref} {...bodyProps}>
+          {resolveYextEntityField(document, text)}
+        </Body>
+      </EntityField>
+    );
+  }
+);
+
+BodyText.displayName = "BodyText";
+
+const bodyTextFields: Fields<BodyTextProps> = {
+  text: YextEntityFieldSelector({
+    label: "Entity Field",
+    filter: {
+      types: ["type.string"],
+    },
+  }),
+  textSize: {
+    label: "Text Size",
+    type: "number",
+    min: 1,
+  },
+  fontWeight: {
+    label: "Font Weight",
+    type: "select",
+    options: [
+      { label: "Default", value: "default" },
+      { label: "Thin", value: "thin" },
+      { label: "Extra Light", value: "extralight" },
+      { label: "Light", value: "light" },
+      { label: "Normal", value: "normal" },
+      { label: "Medium", value: "medium" },
+      { label: "Semibold", value: "semibold" },
+      { label: "Bold", value: "bold" },
+      { label: "Extrabold", value: "extrabold" },
+      { label: "Black", value: "black" },
+    ],
+  },
+  color: {
+    label: "Color",
+    type: "select",
+    options: [
+      { label: "Default", value: "default" },
+      { label: "Primary", value: "primary" },
+      { label: "Secondary", value: "secondary" },
+      { label: "Accent", value: "accent" },
+      { label: "Text", value: "text" },
+      { label: "Background", value: "background" },
+    ],
+  },
+  textTransform: {
+    label: "Text Transform",
+    type: "select",
+    options: [
+      { label: "None", value: "none" },
+      { label: "Uppercase", value: "uppercase" },
+      { label: "Lowercase", value: "lowercase" },
+      { label: "Capitalize", value: "capitalize" },
+    ],
+  },
+};
+
+export const BodyTextComponent: ComponentConfig<BodyTextProps> = {
+  label: "Body Text",
+  fields: bodyTextFields,
+  defaultProps: {
+    text: {
+      field: "",
+      constantValue: "Text",
+      constantValueEnabled: true,
+    },
+    fontWeight: "default",
+    color: "default",
+    textTransform: "none",
+  },
+  render: (props) => <BodyText {...props} />,
+};
+
+export { BodyText, bodyVariants };
---
+++ src/components/puck/atoms/body.tsx
@@ -0,0 +1,79 @@
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "../../../internal/utils/cn.ts";
+
+// Define the variants for the body component
+const bodyVariants = cva("components", {
+  variants: {
+    fontWeight: {
+      default: "",
+      thin: "ve-font-thin",
+      extralight: "ve-font-extralight",
+      light: "ve-font-light",
+      normal: "ve-font-normal",
+      medium: "ve-font-medium",
+      semibold: "ve-font-semibold",
+      bold: "ve-font-bold",
+      extrabold: "ve-font-extrabold",
+      black: "ve-font-black",
+    },
+    color: {
+      default: "",
+      primary: "text-primary",
+      secondary: "text-secondary",
+      accent: "text-accent",
+      text: "text-text",
+      background: "text-primary-background",
+    },
+    textTransform: {
+      none: "",
+      uppercase: "ve-uppercase",
+      lowercase: "ve-lowercase",
+      capitalize: "ve-capitalize",
+    },
+  },
+  defaultVariants: {
+    fontWeight: "default",
+    color: "default",
+    textTransform: "none",
+  },
+});
+
+// Omit 'color' from HTMLAttributes<HTMLParagraphElement> to avoid conflict
+export interface BodyProps
+  extends Omit<React.HTMLAttributes<HTMLParagraphElement>, "color">,
+    VariantProps<typeof bodyVariants> {
+  textSize?: number;
+}
+
+const Body = React.forwardRef<HTMLParagraphElement, BodyProps>(
+  (
+    { className, textSize, fontWeight, color, textTransform, ...props },
+    ref
+  ) => {
+    const dynamicStyles = {
+      fontSize: textSize ? textSize + "px" : undefined,
+    };
+
+    return (
+      <p
+        className={cn(
+          bodyVariants({
+            fontWeight,
+            color,
+            textTransform,
+            className,
+          })
+        )}
+        ref={ref}
+        style={dynamicStyles ?? ""}
+        {...props}
+      >
+        {props.children}
+      </p>
+    );
+  }
+);
+Body.displayName = "Body";
+
+export { Body, bodyVariants };
