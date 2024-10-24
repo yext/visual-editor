200f8fe60a688b319dc2558194e3abc86d4772ce
Alexis Sanehisa <106991746+asanehisa@users.noreply.github.com>
2024-10-24 15:28:37 -0400

    Revert "feat: add header text component (#103)"
    This reverts commit 3ddb00d2538d69fd937c1428ee8ec024954f284e.

--- src/components/puck/index.ts
+++ src/components/puck/index.ts
@@ -1,3 +1,2 @@
 export * from "./atoms/index.ts";
-export { HeadingTextComponent, type HeadingTextProps } from "./HeadingText.tsx";
 export { ImageWrapperComponent, type ImageWrapperProps } from "./Image.tsx";
