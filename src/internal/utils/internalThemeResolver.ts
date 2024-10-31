085c673a0da86e944b8a50219ef12c6dd8bbbeb5
Ben Life <77246839+benlife5@users.noreply.github.com>
2024-10-31 14:26:55 -0400

    refactor: new theme.config format (#126)
    - allows the top level keys of theme.config to be any string -> allows
    custom organization in the TM UI
    - reshapes the theme.config when dumping into tailwind.config, organized
    by the plugin value
    - no longer allows unlimited depth style objects, each section of the
    theme.config can only have direct child styles or one-deep StyleGroups
    if that group shares the same plugin
    - classes created follow the format
    `[tailwindUtility]-[sectionName]-[styleName]-[subStyleName?]`
    - css variables created follow the format
    `--[pluginName]-[sectionName]-[styleName]-[subStyleName?]`
    
    Starter repo PR that uses this:
    https://github.com/YextSolutions/pages-visual-editor-starter/pull/159
    
    ![Screenshot 2024-10-30 at 1 41
    37 PM](https://github.com/user-attachments/assets/7ca75bfa-a23b-49c3-ab30-8c6ba8e97f43)

--- src/internal/utils/internalThemeResolver.ts
+++ src/internal/utils/internalThemeResolver.ts
@@ -1,16 +1,13 @@
-import {
-  ParentStyle,
-  ThemeConfig,
-  SavedTheme,
-} from "../../utils/themeResolver.ts";
+import { ThemeConfig, ThemeConfigSection } from "../../utils/themeResolver.ts";
+import { ThemeData } from "../types/themeData.ts";
 
 // internalThemeResolver returns a mapping of css variable names to their values
 // using the themeConfig to create the list of variable names and applying the saved value
 // if it exists for each one. If it does not exist, it falls back on the themeConfig's default field.
 export const internalThemeResolver = (
   themeConfig: ThemeConfig,
-  savedTheme: SavedTheme | undefined
-): SavedTheme => {
+  savedTheme: ThemeData | undefined
+): ThemeData => {
   const themeValues = generateCssVariablesFromThemeConfig(themeConfig);
 
   if (!savedTheme) {
@@ -25,65 +22,70 @@ export const internalThemeResolver = (
   return themeValues;
 };
 
-const extractCssVariablesFromThemeConfig = (
-  parentStyle: ParentStyle,
-  parentKey: string,
-  result: SavedTheme
-) => {
-  Object.entries(parentStyle.styles).forEach(([styleKey, style]) => {
-    if ("default" in style) {
-      if (style.type === "number") {
-        result[`--${parentKey}-${styleKey}`] = `${style.default}px`;
-      } else {
-        result[`--${parentKey}-${styleKey}`] = style.default;
-      }
-    } else {
-      extractCssVariablesFromThemeConfig(
-        style,
-        `${parentKey}-${styleKey}`,
-        result
-      );
-    }
-  });
-  return result;
-};
-
 // generateCssVariablesFromThemeConfig flattens the themeConfig, creating an object
 // mapping css variable names to the themeConfig's default values
 export const generateCssVariablesFromThemeConfig = (
   themeConfig: ThemeConfig
 ) => {
-  const result: SavedTheme = {};
-  for (const category in themeConfig) {
-    extractCssVariablesFromThemeConfig(themeConfig[category], category, result);
-  }
-  return result;
-};
+  const defaultValues: ThemeData = {};
+  for (const themeSectionKey in themeConfig) {
+    const themeSection = themeConfig[themeSectionKey].styles;
 
-type PuckThemeFields = {
-  [key: string]: string | PuckThemeFields;
+    for (const styleKey in themeSection) {
+      const style = themeSection[styleKey];
+
+      if ("default" in style) {
+        defaultValues[`--${style.plugin}-${themeSectionKey}-${styleKey}`] =
+          typeof style.default === "number"
+            ? `${style.default}px`
+            : style.default;
+      } else if ("styles" in style) {
+        for (const subkey in style.styles) {
+          defaultValues[
+            `--${style.plugin}-${themeSectionKey}-${styleKey}-${subkey}`
+          ] =
+            typeof style.styles[subkey].default === "number"
+              ? `${style.styles[subkey].default}px`
+              : style.styles[subkey].default;
+        }
+      }
+    }
+  }
+  return defaultValues;
 };
 
 // generateCssVariablesFromPuckFields flattens the puck sidebar fields, creating an object
 // mapping css variable names to the current puck values
 export const generateCssVariablesFromPuckFields = (
-  fields: PuckThemeFields,
-  parentKey = "",
-  result: SavedTheme = {}
+  fields: Record<string, any>,
+  themeSectionKey: string,
+  themeSection: ThemeConfigSection
 ) => {
-  for (const fieldKey in fields) {
-    const fieldValue = fields[fieldKey];
-    if (typeof fieldValue === "object") {
-      generateCssVariablesFromPuckFields(
-        fieldValue,
-        `${parentKey}-${fieldKey}`,
-        result
-      );
-    } else if (typeof fieldValue === "number") {
-      result[`--${parentKey}-${fieldKey}`] = fieldValue + "px";
+  const result: ThemeData = {};
+
+  for (const styleKey in themeSection.styles) {
+    const style = themeSection.styles[styleKey];
+    if ("default" in style) {
+      if (style.type === "number") {
+        result[`--${style.plugin}-${themeSectionKey}-${styleKey}`] =
+          fields[styleKey] + "px";
+      } else {
+        result[`--${style.plugin}-${themeSectionKey}-${styleKey}`] =
+          fields[styleKey];
+      }
     } else {
-      result[`--${parentKey}-${fieldKey}`] = fieldValue;
+      for (const subkey in style.styles) {
+        const substyle = style.styles[subkey];
+        if (substyle.type === "number") {
+          result[`--${style.plugin}-${themeSectionKey}-${styleKey}-${subkey}`] =
+            fields[styleKey][subkey] + "px";
+        } else {
+          result[`--${style.plugin}-${themeSectionKey}-${styleKey}-${subkey}`] =
+            fields[styleKey][subkey];
+        }
+      }
     }
   }
+
   return result;
 };
