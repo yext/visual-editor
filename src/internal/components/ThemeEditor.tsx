80d64533c18cd4cb80ee8d430cae1714397e03a9
Jacob Wartofsky <jwartofsky@yext.com>
2024-11-04 15:28:49 -0500

    fix notices

--- src/internal/components/ThemeEditor.tsx
+++ src/internal/components/ThemeEditor.tsx
@@ -87,6 +87,7 @@ export const ThemeEditor = (props: ThemeEditorProps) => {
 
     // Only load Content data for theme mode
     if (visualConfigurationData) {
+      devLogger.logData("VISUAL_CONFIGURATION_DATA", visualConfigurationData);
       setPuckInitialHistory({
         histories: [{ id: "root", state: { data: visualConfigurationData } }],
         appendData: false,
