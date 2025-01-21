yext/visual-editor

commit 9333bfbc7c06b8d183ac032936b1e847136ea11f

refactor: update constant value field styling (#193)
~~Will send to Aaron for UI approval~~  👍

Text - Entity Field

![text-entity](https://github.com/user-attachments/assets/f71b04ef-cf31-444c-b3ae-b3791e6f37e8)

Text - Constant Value

![text-constant](https://github.com/user-attachments/assets/1043e4f9-26c5-446d-885f-15b4633f444a)

Phone - Entity Field

![phone-entity](https://github.com/user-attachments/assets/22c500b1-18b8-45f9-809a-abcda4b4bef8)

Phone - Constant Value

![phone-constant](https://github.com/user-attachments/assets/ca11af70-cc1d-4c99-b543-5cd78c00fc59)

Image - Entity Field

![image-entity](https://github.com/user-attachments/assets/7ad09720-a91b-42f3-9ab8-3b358280ea68)

Image - Constant Value

![image-constant](https://github.com/user-attachments/assets/50a3c533-7a99-40ce-be1e-98ad56852553)

Address - Entity Field

![address-entity](https://github.com/user-attachments/assets/c43f910a-505a-4375-9618-81d4d23cddc3)

Address - Constant Value

![address-constant](https://github.com/user-attachments/assets/86651c01-d8af-4f81-9d75-aa54a0b3efdb)

Call to Action - Entity Field

![cta-entity](https://github.com/user-attachments/assets/e399862e-996a-4572-bd62-bdc2f26ddf21)

Call to Action - Constant Value

![cta-constant](https://github.com/user-attachments/assets/27793bdc-3bd2-4364-9069-a870e5a57bd9)

Text List - Entity Field

![text-list-entity](https://github.com/user-attachments/assets/f70f2fe9-42d2-412f-b7d2-b587d80d9404)

Text List - Constant Value

![text-list-constant](https://github.com/user-attachments/assets/af8b261b-82ea-4bbe-8a17-e5e97c1bc888)

Hide "Constant Value" option for field types that do not have a constant
value input

![unsupported](https://github.com/user-attachments/assets/67e692ea-c09e-49e4-b541-f31b6519f5db)

src/components/editor/YextEntityFieldSelector.tsx
@@ -14,6 +14,7 @@ import { TEXT_CONSTANT_CONFIG } from "../../internal/puck/constant-value-fields/
 import { ADDRESS_CONSTANT_CONFIG } from "../../internal/puck/constant-value-fields/Address.tsx";
 import { TEXT_LIST_CONSTANT_CONFIG } from "../../internal/puck/constant-value-fields/TextList.tsx";
 import { CTA_CONSTANT_CONFIG } from "../../internal/puck/constant-value-fields/CallToAction.tsx";
+import { PHONE_CONSTANT_CONFIG } from "../../internal/puck/constant-value-fields/Phone.tsx";
 
 const devLogger = new DevLogger();
 
@@ -31,10 +32,14 @@ export type RenderYextEntityFieldSelectorProps<T extends Record<string, any>> =
 
 const TYPE_TO_CONSTANT_CONFIG: Record<string, Field<any>> = {
   "type.string": TEXT_CONSTANT_CONFIG,
-  "type.phone": TEXT_CONSTANT_CONFIG,
+  "type.phone": PHONE_CONSTANT_CONFIG,
   "type.image": IMAGE_CONSTANT_CONFIG,
   "type.address": ADDRESS_CONSTANT_CONFIG,
   "type.cta": CTA_CONSTANT_CONFIG,
+};
+
+const LIST_TYPE_TO_CONSTANT_CONFIG: Record<string, Field<any>> = {
+  "type.string": TEXT_LIST_CONSTANT_CONFIG,
 };
 
 const getConstantConfigFromType = (
@@ -42,7 +47,7 @@ const getConstantConfigFromType = (
   isList: boolean
 ): Field<any> | undefined => {
   if (isList) {
-    return TEXT_LIST_CONSTANT_CONFIG;
+    return LIST_TYPE_TO_CONSTANT_CONFIG[type];
   }
   const constantConfig = TYPE_TO_CONSTANT_CONFIG[type];
   if (!constantConfig) {
@@ -102,6 +107,7 @@ export const YextEntityFieldSelector = <T extends Record<string, any>, U>(
       return (
         <>
           <ConstantValueModeToggler
+            fieldTypeFilter={props.filter.types}
             constantValueEnabled={value?.constantValueEnabled}
             toggleConstantValueEnabled={toggleConstantValueEnabled}
           />
@@ -126,17 +132,26 @@ export const YextEntityFieldSelector = <T extends Record<string, any>, U>(
 };
 
 const ConstantValueModeToggler = ({
+  fieldTypeFilter,
   constantValueEnabled,
   toggleConstantValueEnabled,
 }: {
+  fieldTypeFilter: EntityFieldTypes[];
   constantValueEnabled: boolean;
   toggleConstantValueEnabled: (constantValueEnabled: boolean) => void;
 }) => {
   const random = Math.floor(Math.random() * 999999);
   const entityButtonId = `ve-use-entity-value-${random}`;
   const constantButtonId = `ve-use-constant-value-${random}`;
+
+  const constantValueInputSupported = fieldTypeFilter.some(
+    (fieldType) =>
+      Object.keys(TYPE_TO_CONSTANT_CONFIG).includes(fieldType) ||
+      Object.keys(LIST_TYPE_TO_CONSTANT_CONFIG).includes(fieldType)
+  );
+
   return (
-    <div className="ve-mb-2 ve-w-full">
+    <div className="ve-w-full">
       <RadioGroup defaultValue={constantValueEnabled?.toString() ?? "false"}>
         <div className="ve-flex ve-items-center ve-space-x-2">
           <RadioGroupItem
@@ -146,14 +161,16 @@ const ConstantValueModeToggler = ({
           />
           <Label htmlFor={entityButtonId}>Use Entity Value</Label>
         </div>
-        <div className="ve-flex ve-items-center ve-space-x-2">
-          <RadioGroupItem
-            value="true"
-            id={constantButtonId}
-            onClick={() => toggleConstantValueEnabled(true)}
-          />
-          <Label htmlFor={constantButtonId}>Use Constant Value</Label>
-        </div>
+        {constantValueInputSupported && (
+          <div className="ve-flex ve-items-center ve-space-x-2">
+            <RadioGroupItem
+              value="true"
+              id={constantButtonId}
+              onClick={() => toggleConstantValueEnabled(true)}
+            />
+            <Label htmlFor={constantButtonId}>Use Constant Value</Label>
+          </div>
+        )}
       </RadioGroup>
     </div>
   );
@@ -177,21 +194,19 @@ const ConstantValueInput = <T extends Record<string, any>>({
   );
 
   return (
-    <FieldLabel label={"Constant Value"} className="entityField-constantValue">
-      {constantFieldConfig && (
-        <AutoField
-          onChange={(newConstantValue) =>
-            onChange({
-              field: value?.field ?? "",
-              constantValue: newConstantValue,
-              constantValueEnabled: true,
-            })
-          }
-          value={value?.constantValue}
-          field={constantFieldConfig}
-        />
-      )}
-    </FieldLabel>
+    constantFieldConfig && (
+      <AutoField
+        onChange={(newConstantValue) =>
+          onChange({
+            field: value?.field ?? "",
+            constantValue: newConstantValue,
+            constantValueEnabled: true,
+          })
+        }
+        value={value?.constantValue}
+        field={constantFieldConfig}
+      />
+    )
   );
 };
 
@@ -204,7 +219,10 @@ const EntityFieldInput = <T extends Record<string, any>>({
   const filteredEntityFields = getFilteredEntityFields(filter);
 
   return (
-    <FieldLabel label={label ?? "Entity Field"} className="ve-mt-2.5">
+    <FieldLabel
+      label={label ?? "Entity Field"}
+      className="ve-inline-block ve-w-full ve-pt-4"
+    >
       <AutoField
         field={{
           type: "select",

src/internal/puck/constant-value-fields/ConstantField.tsx
@@ -30,7 +30,7 @@ function ConstantField<T extends Record<string, any>>(
   return (
     <FieldLabel
       label={fieldProps.label}
-      className="ve-inline-block ve-pt-4"
+      className="ve-inline-block ve-pt-4 w-full"
       key={`constant-${fieldProps.field}-label`}
     >
       <AutoField

src/internal/puck/constant-value-fields/Phone.tsx
@@ -1,7 +1,7 @@
 import { CustomField } from "@measured/puck";
 import { ConstantFields } from "./ConstantField.tsx";
 
-export const TEXT_CONSTANT_CONFIG: CustomField = {
+export const PHONE_CONSTANT_CONFIG: CustomField = {
   type: "custom",
   render: ({ onChange, value }) => {
     return ConstantFields({
@@ -9,8 +9,8 @@ export const TEXT_CONSTANT_CONFIG: CustomField = {
       value: value,
       fields: [
         {
-          label: "Text",
-          field: "text",
+          label: "Phone Number",
+          field: "phone",
           fieldType: "text",
         },
       ],

src/internal/puck/constant-value-fields/TextList.tsx
@@ -28,22 +28,16 @@ export const TEXT_LIST_CONSTANT_CONFIG: CustomField<string[]> = {
     };
 
     return (
-      <div>
+      <div className="ve-inline-block ve-pt-4 w-full">
         {localItems.map((item, index) => (
-          <div
-            key={index}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              marginBottom: "8px",
-              gap: "4px",
-            }}
-          >
-            <AutoField
-              field={{ type: "text" }}
-              value={item}
-              onChange={(itemValue) => updateItem(index, itemValue)}
-            />
+          <div key={index} className="ve-flex ve-items-center ve-mb-2 ve-gap-2">
+            <div className="ve-grow">
+              <AutoField
+                field={{ type: "text" }}
+                value={item}
+                onChange={(itemValue) => updateItem(index, itemValue)}
+              />
+            </div>
             <span style={{ color: TEXT_LIST_BUTTON_COLOR }}>
               <IconButton
                 onClick={() => removeItem(index)}

src/internal/puck/ui/label.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "../../../utils/cn.ts";
 
 const labelVariants = cva(
-  "ve-text-sm ve-font-medium ve-leading-none peer-disabled:ve-cursor-not-allowed peer-disabled:ve-opacity-70"
+  "ve-text-sm ve-font-medium ve-leading-none ve-cursor-pointer peer-disabled:ve-cursor-not-allowed peer-disabled:ve-opacity-70"
 );
 
 const Label = React.forwardRef<

commit af2a77f406e74b84cfbec9e8366c2efdff2a52a6

fix: error in constant text fields (#199)
Fixes a bug I introduced in #193 
Was trying to use the custom ConstantField styling for all constant
value inputs, but missed that the ConstantField function depended on the
the field being an object type. This PR reverts Phone and Text to being
text types but adds a case to render a styled FieldLabel for them.

src/components/editor/YextEntityFieldSelector.tsx
@@ -193,8 +193,27 @@ const ConstantValueInput = <T extends Record<string, any>>({
     !!filter.includeListsOnly
   );
 
-  return (
-    constantFieldConfig && (
+  if (!constantFieldConfig) {
+    return;
+  }
+
+  return constantFieldConfig.type === "custom" ? (
+    <AutoField
+      onChange={(newConstantValue) =>
+        onChange({
+          field: value?.field ?? "",
+          constantValue: newConstantValue,
+          constantValueEnabled: true,
+        })
+      }
+      value={value?.constantValue}
+      field={constantFieldConfig}
+    />
+  ) : (
+    <FieldLabel
+      label={constantFieldConfig.label ?? "Value"}
+      className="ve-inline-block ve-pt-4 w-full"
+    >
       <AutoField
         onChange={(newConstantValue) =>
           onChange({
@@ -206,7 +225,7 @@ const ConstantValueInput = <T extends Record<string, any>>({
         value={value?.constantValue}
         field={constantFieldConfig}
       />
-    )
+    </FieldLabel>
   );
 };
 

src/internal/puck/constant-value-fields/ConstantField.tsx
@@ -17,7 +17,7 @@ export function ConstantFields<T extends Record<string, any>>(
 ): ReactElement {
   const fields: ReactElement[] = [];
   props.fields.forEach((field) => {
-    fields.push(ConstantField(props, field));
+    fields.push(ConstantField<T>(props, field));
   });
 
   return <>{fields}</>;

src/internal/puck/constant-value-fields/Phone.tsx
@@ -1,19 +1,6 @@
-import { CustomField } from "@measured/puck";
-import { ConstantFields } from "./ConstantField.tsx";
+import { TextField } from "@measured/puck";
 
-export const PHONE_CONSTANT_CONFIG: CustomField = {
-  type: "custom",
-  render: ({ onChange, value }) => {
-    return ConstantFields({
-      onChange: onChange,
-      value: value,
-      fields: [
-        {
-          label: "Phone Number",
-          field: "phone",
-          fieldType: "text",
-        },
-      ],
-    });
-  },
+export const PHONE_CONSTANT_CONFIG: TextField = {
+  type: "text",
+  label: "Phone",
 };

src/internal/puck/constant-value-fields/Text.tsx
@@ -1,19 +1,6 @@
-import { CustomField } from "@measured/puck";
-import { ConstantFields } from "./ConstantField.tsx";
+import { TextField } from "@measured/puck";
 
-export const TEXT_CONSTANT_CONFIG: CustomField = {
-  type: "custom",
-  render: ({ onChange, value }) => {
-    return ConstantFields({
-      onChange: onChange,
-      value: value,
-      fields: [
-        {
-          label: "Text",
-          field: "text",
-          fieldType: "text",
-        },
-      ],
-    });
-  },
+export const TEXT_CONSTANT_CONFIG: TextField = {
+  type: "text",
+  label: "Text",
 };