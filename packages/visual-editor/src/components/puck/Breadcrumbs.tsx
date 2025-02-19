ad3459f2048f1694d0ed9c22711213b76ff4cd3e
Alexis Sanehisa <asanehisa@yext.com>
2025-02-19 14:50:07 -0500

    comments

--- packages/visual-editor/src/components/puck/Breadcrumbs.tsx
+++ packages/visual-editor/src/components/puck/Breadcrumbs.tsx
@@ -1,14 +1,13 @@
-import { Link } from "@yext/pages-components";
 import { useDocument } from "../../index.js";
 import { ComponentConfig } from "@measured/puck";
-import { Key } from "react";
+import { MaybeLink } from "./atoms/maybeLink.js";
 
 export type BreadcrumbsProps = {
   separator?: string;
 };
 
 const getDirectoryParents = (
-  document: any
+  document: Record<string, any>
 ): Array<{ slug: string; name: string }> => {
   for (const key in document) {
     if (key.startsWith("dm_directoryParents_")) {
@@ -16,26 +15,6 @@ const getDirectoryParents = (
     }
   }
   return [];
-};
-
-const Breadcrumb = ({
-  name,
-  href,
-  linkClassName,
-}: {
-  name: string;
-  href: string;
-  linkClassName?: string;
-}) => {
-  if (href) {
-    return (
-      <Link href={href} className={linkClassName}>
-        {name}
-      </Link>
-    );
-  } else {
-    return <>{name}</>;
-  }
 };
 
 const BreadcrumbsComponent = (props: BreadcrumbsProps) => {
@@ -48,24 +27,23 @@ const BreadcrumbsComponent = (props: BreadcrumbsProps) => {
       {breadcrumbs?.length && (
         <nav className="my-4" aria-label="Breadcrumb">
           <ol className="components flex flex-wrap text-link-fontSize text-body-color">
-            {breadcrumbs.map(
-              ({ name, slug }: { name: string; slug: string }, idx: Key) => {
-                const isLast = idx === breadcrumbs.length - 1;
-                const href = document.relativePrefixToRoot
-                  ? document.relativePrefixToRoot + slug
-                  : slug;
-                return (
-                  <li key={idx}>
-                    <Breadcrumb
-                      name={name}
-                      href={isLast ? "" : href}
-                      linkClassName="text-link-color underline hover:no-underline"
-                    />
-                    {!isLast && <span className="mx-2">{separator}</span>}
-                  </li>
-                );
-              }
-            )}
+            {breadcrumbs.map(({ name, slug }, idx) => {
+              const isLast = idx === breadcrumbs.length - 1;
+              const href = document.relativePrefixToRoot
+                ? document.relativePrefixToRoot + slug
+                : slug;
+              return (
+                <li key={idx}>
+                  <MaybeLink
+                    href={isLast ? "" : href}
+                    className="text-link-color underline hover:no-underline"
+                  >
+                    {name}
+                  </MaybeLink>
+                  {!isLast && <span className="mx-2">{separator}</span>}
+                </li>
+              );
+            })}
           </ol>
         </nav>
       )}
