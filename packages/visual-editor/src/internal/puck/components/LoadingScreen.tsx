bc99d8205759b4b7aae648dc694343e80a80cdb8
Brian Stephan <bstephan@yext.com>
2025-03-03 23:41:00 -0500

    fix: prevent loading bar from going backwards

--- packages/visual-editor/src/internal/puck/components/LoadingScreen.tsx
+++ packages/visual-editor/src/internal/puck/components/LoadingScreen.tsx
@@ -8,9 +8,17 @@ export type LoadingScreenProps = {
 };
 
 export function LoadingScreen({ progress }: LoadingScreenProps) {
+  // This ensures that the progress bar never goes backwards.
+  const [maxProgress, setMaxProgress] = React.useState(0);
+  React.useEffect(() => {
+    if (progress > maxProgress) {
+      setMaxProgress(progress);
+    }
+  }, [progress, maxProgress]);
+
   return (
     <div className="ve-flex ve-h-screen ve-loading-wrapper ve-w-screen ve-flex-col ve-items-center ve-justify-center">
-      <Progress className="ve-w-1/3 ve-loading-progress" value={progress} />
+      <Progress className="ve-w-1/3 ve-loading-progress" value={maxProgress} />
       <div>Loading Visual Editor...</div>
     </div>
   );
