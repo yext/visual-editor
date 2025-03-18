b8f55213a60999d33cef1b6a9809a77a610f2c5c
Alexis Sanehisa <106991746+asanehisa@users.noreply.github.com>
2025-03-18 13:05:21 -0400

    feat: add yextVisualEditorPlugin() (#252)
    On build, it creates the edit and main files in src/templates/ then
    removes them. While running dev mode, on server generate, it creates the
    edit and main files as well and on server end (or ctrl C) the files are
    removed. This allows devMode to work.
    
    Future followups: 
    - Investigate other ways for `import { yextVisualEditorPlugin } from
    "@yext/visual-editor"; ` to work instead of importing from
    `@yext/visual-editor/plugin`. I ran into issues getting them to resolve
    to same export path due to SSR vs non-SSR handling. Attempted additional
    plugin code to have yextVisualEditorPlugin exported in dist/index.js
    with other code as well as dynamically importing path and fs-extra but
    failed to get devMode and build to work properly.
    - Support dynamically importing themeConfig and allowing no themeConfig
    - Support scope
    - Support library upgrades (puck, etc)

---
+++ packages/visual-editor/src/vite-plugin/plugin.ts
@@ -0,0 +1,64 @@
+import path from "node:path";
+import fs from "fs-extra";
+import { Plugin } from "vite";
+import mainTemplate from "./templates/main.tsx?raw";
+import editTemplate from "./templates/edit.tsx?raw";
+
+// files to generate
+const virtualFiles: Record<string, string> = {
+  "src/templates/main.tsx": mainTemplate,
+  "src/templates/edit.tsx": editTemplate,
+};
+
+export const yextVisualEditorPlugin = (): Plugin => {
+  let isBuildMode = false;
+  const filesToCleanup: string[] = [];
+
+  const generateFiles = () => {
+    Object.entries(virtualFiles).forEach(([fileName, content]) => {
+      const filePath = path.join(process.cwd(), fileName);
+      filesToCleanup.push(filePath);
+      fs.mkdirSync(path.dirname(filePath), { recursive: true });
+      fs.writeFileSync(filePath, content);
+    });
+  };
+
+  const cleanupFiles = () => {
+    filesToCleanup.forEach((filePath) => {
+      fs.rmSync(filePath, { force: true });
+    });
+  };
+
+  // cleanup on interruption (ctrl + C)
+  process.on("SIGINT", () => {
+    cleanupFiles();
+    process.nextTick(() => process.exit(0));
+  });
+
+  process.on("SIGTERM", () => {
+    cleanupFiles();
+    process.nextTick(() => process.exit(0));
+  });
+
+  return {
+    name: "vite-plugin-yext-visual-editor",
+    config(_, { command }) {
+      isBuildMode = command === "build";
+    },
+    buildStart() {
+      generateFiles();
+    },
+    buildEnd() {
+      if (!isBuildMode) {
+        return;
+      }
+      cleanupFiles();
+    },
+    closeBundle() {
+      if (!isBuildMode) {
+        return;
+      }
+      cleanupFiles();
+    },
+  };
+};
