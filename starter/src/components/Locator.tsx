e378260a077f3f98d98cdc8a8b2db5ff88c26722
Jacob Fondriest <jfondriest@yext.com>
2025-03-18 14:10:41 -0400

    feat: FilterSearch Bar for Locator component
    This PR inserts a basic FilterSearch bar into the Locator
    component. It does not do anything with the results.
    J=WAT-4698
    TEST=manual
    
    Ran locally and saw filter searches execute

--- starter/src/components/Locator.tsx
+++ starter/src/components/Locator.tsx
@@ -1,12 +1,26 @@
 import { ComponentConfig, Fields } from "@measured/puck";
+import { FilterSearch } from "@yext/search-ui-react";
+import {
+  CloudChoice,
+  CloudRegion,
+  Environment,
+  provideHeadless,
+  SearchHeadlessProvider,
+} from "@yext/search-headless-react";
+import * as React from "react";
 
 type LocatorProps = {
   text: string;
+  apiKey: string;
 };
 
 const fields: Fields<LocatorProps> = {
   text: {
     label: "Text",
+    type: "text",
+  },
+  apiKey: {
+    label: "Api Key",
     type: "text",
   },
 };
@@ -15,13 +29,34 @@ const LocatorComponent: ComponentConfig<LocatorProps> = {
   fields,
   defaultProps: {
     text: "Some Placeholder",
+    apiKey: "Fake Key",
   },
   label: "Locator",
   render: (props) => <Locator {...props} />,
 };
 
-const Locator: React.FC<LocatorProps> = ({ text }) => {
-  return <div>{text}</div>;
+const Locator: React.FC<LocatorProps> = (props) => {
+  const config = {
+    apiKey: props.apiKey,
+    experienceKey: "jacob-test",
+    locale: "en",
+    experienceVersion: "STAGING",
+    businessId: 70452,
+    cloudRegion: CloudRegion.US,
+    cloudChoice: CloudChoice.GLOBAL_MULTI,
+    environment: Environment.PROD,
+  };
+  const searcher = provideHeadless(config);
+  searcher.setVertical("locations");
+  return (
+    <SearchHeadlessProvider searcher={searcher}>
+      <FilterSearch
+        searchFields={[
+          { fieldApiName: "builtin.location", entityType: "location" },
+        ]}
+      ></FilterSearch>
+    </SearchHeadlessProvider>
+  );
 };
 
 export { type LocatorProps, LocatorComponent as Locator };
