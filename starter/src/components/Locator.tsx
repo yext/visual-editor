542168a3018b0151feea76c04759db4c6d85ba98
Fondryext <160865254+Fondryext@users.noreply.github.com>
2025-03-19 14:31:52 -0400

    feat: add FilterSearch Bar for Locator component (#264)
    This PR inserts a basic FilterSearch bar into the Locator component. It
    does not do anything with the results.
    J=WAT-4698
    TEST=manual
    
    Ran locally and saw filter searches execute

--- starter/src/components/Locator.tsx
+++ starter/src/components/Locator.tsx
@@ -1,12 +1,22 @@
 import { ComponentConfig, Fields } from "@measured/puck";
+import { FilterSearch } from "@yext/search-ui-react";
+import {
+  CloudChoice,
+  CloudRegion,
+  Environment,
+  provideHeadless,
+  SearchHeadlessProvider,
+} from "@yext/search-headless-react";
+import * as React from "react";
+import { useDocument } from "@yext/visual-editor";
 
 type LocatorProps = {
-  text: string;
+  apiKey: string;
 };
 
 const fields: Fields<LocatorProps> = {
-  text: {
-    label: "Text",
+  apiKey: {
+    label: "API Key",
     type: "text",
   },
 };
@@ -14,14 +24,37 @@ const fields: Fields<LocatorProps> = {
 const LocatorComponent: ComponentConfig<LocatorProps> = {
   fields,
   defaultProps: {
-    text: "Some Placeholder",
+    apiKey: "Fake Key",
   },
   label: "Locator",
   render: (props) => <Locator {...props} />,
 };
 
-const Locator: React.FC<LocatorProps> = ({ text }) => {
-  return <div>{text}</div>;
+const Locator: React.FC<LocatorProps> = ({ apiKey }) => {
+  const document: {
+    businessId: number;
+  } = useDocument();
+  const config = {
+    apiKey,
+    experienceKey: "jacob-test",
+    locale: "en",
+    experienceVersion: "STAGING",
+    businessId: document.businessId,
+    cloudRegion: CloudRegion.US,
+    cloudChoice: CloudChoice.GLOBAL_MULTI,
+    environment: Environment.PROD,
+  };
+  const searcher = provideHeadless(config);
+  searcher.setVertical("locations");
+  return (
+    <SearchHeadlessProvider searcher={searcher}>
+      <FilterSearch
+        searchFields={[
+          { fieldApiName: "builtin.location", entityType: "location" },
+        ]}
+      ></FilterSearch>
+    </SearchHeadlessProvider>
+  );
 };
 
 export { type LocatorProps, LocatorComponent as Locator };
