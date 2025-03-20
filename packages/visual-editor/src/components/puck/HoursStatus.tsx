65b50a61c265fab5d81a0f663f601f069480795a
colton-demetriou <cdemetriou@yext.com>
2025-03-20 17:27:23 -0400

    use h5 for hours status text

--- packages/visual-editor/src/components/puck/HoursStatus.tsx
+++ packages/visual-editor/src/components/puck/HoursStatus.tsx
@@ -81,7 +81,7 @@ const HoursStatusWrapper: React.FC<HoursStatusProps> = ({
       <HoursStatus
         hours={hours}
         className={themeManagerCn(
-          "components font-semibold mb-2 font-body-fontFamily",
+          "components mb-2 font-heading5-fontWeight font-heading5-fontFamily",
           className
         )}
         currentTemplate={showCurrentStatus ? undefined : () => <></>}
