6bb8fb7ecf088ce03cdb124c41230a67a29d84dc
Ben Life <blife@yext.com>
2025-03-24 16:42:29 -0400

    auto generated colors

--- packages/visual-editor/src/utils/themeConfigOptions.ts
+++ packages/visual-editor/src/utils/themeConfigOptions.ts
@@ -28,3 +28,61 @@ export const getSpacingOptions = () => {
     };
   });
 };
+
+export type BackgroundColorOption = {
+  bgColor: string;
+  textColor: string;
+};
+
+export const getBackgroundColorOptions = () => {
+  return backgroundColors;
+};
+
+const backgroundColors: { label: string; value: BackgroundColorOption }[] = [
+  {
+    label: "Background 1",
+    value: { bgColor: "bg-white", textColor: "text-black" },
+  },
+  {
+    label: "Background 2",
+    value: {
+      bgColor: "bg-palette-primary-light",
+      textColor: "text-black",
+    },
+  },
+  {
+    label: "Background 3",
+    value: {
+      bgColor: "bg-palette-secondary-light",
+      textColor: "text-black",
+    },
+  },
+  {
+    label: "Background 4",
+    value: {
+      bgColor: "bg-palette-tertiary-light",
+      textColor: "text-black",
+    },
+  },
+  {
+    label: "Background 5",
+    value: {
+      bgColor: "bg-palette-quaternary-light",
+      textColor: "text-black",
+    },
+  },
+  {
+    label: "Background 6",
+    value: {
+      bgColor: "bg-palette-primary-dark",
+      textColor: "text-white",
+    },
+  },
+  {
+    label: "Background 7",
+    value: {
+      bgColor: "bg-palette-secondary-dark",
+      textColor: "text-white",
+    },
+  },
+];
