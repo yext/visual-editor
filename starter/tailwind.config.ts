f1508e21af29a0a27e82dd2d86bec2914d960512
Ben Life <blife@yext.com>
2025-03-24 17:02:59 -0400

    add grays

--- starter/tailwind.config.ts
+++ starter/tailwind.config.ts
@@ -23,6 +23,16 @@ export default {
             "hsl(from var(--colors-palette-primary) h s 20)",
           "palette-secondary-dark":
             "hsl(from var(--colors-palette-secondary) h s 20)",
+          gray: {
+            100: "#F9F9F9",
+            200: "EDEDED",
+            300: "#D4D4D4",
+            400: "#BABABA",
+            500: "#7A7A7A",
+            600: "#2B2B2B",
+            800: "#1F1F1F",
+            900: "#121212",
+          },
         },
       },
       themeConfig,
