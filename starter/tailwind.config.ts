c70469425b74344c9b091757ee069d6e56c6c4f9
Ben Life <blife@yext.com>
2025-03-24 16:42:29 -0400

    auto generated colors

--- starter/tailwind.config.ts
+++ starter/tailwind.config.ts
@@ -8,7 +8,25 @@ export default {
     "./node_modules/@yext/visual-editor/dist/**/*.js",
   ],
   theme: {
-    extend: themeResolver({}, themeConfig),
+    extend: themeResolver(
+      {
+        colors: {
+          "palette-primary-light":
+            "hsl(from var(--colors-palette-primary) h s 98)",
+          "palette-secondary-light":
+            "hsl(from var(--colors-palette-secondary) h s 98)",
+          "palette-tertiary-light":
+            "hsl(from var(--colors-palette-tertiary) h s 98)",
+          "palette-quaternary-light":
+            "hsl(from var(--colors-palette-quaternary) h s 98)",
+          "palette-primary-dark":
+            "hsl(from var(--colors-palette-primary) h s 20)",
+          "palette-secondary-dark":
+            "hsl(from var(--colors-palette-secondary) h s 20)",
+        },
+      },
+      themeConfig,
+    ),
   },
   plugins: [],
 } satisfies Config;
