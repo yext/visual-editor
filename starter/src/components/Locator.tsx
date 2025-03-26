c1e202a59f2fc9512d30b06dcbfbed3dc0e1331d
Fondryext <160865254+Fondryext@users.noreply.github.com>
2025-03-26 10:53:29 -0400

    feat: add basic result cards (#277)
    This PR adds basic result cards, still in need of styling. It also
    adjusts how the SearchHeadlessProvider is created, so that the
    call to provideHeadless is not inside of a component. This may need
    adjusting.
    
    J=WAT-4699
    TEST=manual
    
    Results are populated, albeit unstyled. No rerendering issues

--- starter/src/components/Locator.tsx
+++ starter/src/components/Locator.tsx
@@ -12,34 +12,49 @@ import {
   useSearchState,
 } from "@yext/search-headless-react";
 import * as React from "react";
+import { cva, VariantProps } from "class-variance-authority";
+import { BasicSelector, themeManagerCn } from "@yext/visual-editor";
 
-type LocatorProps = {
-  fieldApiName: string;
-  entityType: string;
-};
+const DEFAULT_FIELD = "builtin.location";
+const DEFAULT_ENTITY_TYPE = "location";
 
-const fields: Fields<LocatorProps> = {
-  fieldApiName: {
-    label: "Field Name",
-    type: "text",
+const locatorVariants = cva("", {
+  variants: {
+    backgroundColor: {
+      default: "bg-locator-backgroundColor",
+      primary: "bg-palette-primary",
+      secondary: "bg-palette-secondary",
+      accent: "bg-palette-accent",
+      text: "bg-palette-text",
+      background: "bg-palette-background",
+    },
   },
-  entityType: {
-    label: "Entity Type",
-    type: "text",
+  defaultVariants: {
+    backgroundColor: "default",
   },
+});
+
+type LocatorProps = VariantProps<typeof locatorVariants>;
+
+const locatorFields: Fields<LocatorProps> = {
+  backgroundColor: BasicSelector("Background Color", [
+    { label: "Default", value: "default" },
+    { label: "Primary", value: "primary" },
+    { label: "Secondary", value: "secondary" },
+    { label: "Accent", value: "accent" },
+    { label: "Text", value: "text" },
+    { label: "Background", value: "background" },
+  ]),
 };
 
 const LocatorComponent: ComponentConfig<LocatorProps> = {
-  fields,
-  defaultProps: {
-    fieldApiName: "builtin.location",
-    entityType: "location",
-  },
+  fields: locatorFields,
   label: "Locator",
   render: (props) => <Locator {...props} />,
 };
 
-const Locator: React.FC<LocatorProps> = ({ fieldApiName, entityType }) => {
+const Locator: React.FC<LocatorProps> = (props) => {
+  const { backgroundColor } = props;
   const resultCount = useSearchState(
     (state) => state.vertical.resultsCount || 0,
   );
@@ -53,7 +68,7 @@ const Locator: React.FC<LocatorProps> = ({ fieldApiName, entityType }) => {
         kind: "fieldValue",
         fieldId: params.newFilter.fieldId,
         value: params.newFilter.value,
-        matcher: Matcher.Equals,
+        matcher: Matcher.Near,
       },
     };
     searchActions.setStaticFilters([locationFilter]);
@@ -65,13 +80,20 @@ const Locator: React.FC<LocatorProps> = ({ fieldApiName, entityType }) => {
   return (
     <>
       <FilterSearch
-        searchFields={[{ fieldApiName: fieldApiName, entityType: entityType }]}
+        searchFields={[
+          { fieldApiName: DEFAULT_FIELD, entityType: DEFAULT_ENTITY_TYPE },
+        ]}
         onSelect={(params) => handleFilterSelect(params)}
       />
       <div>
         {resultCount > 0 && <VerticalResults CardComponent={StandardCard} />}
         {resultCount === 0 && !searchLoading && (
-          <div className="flex items-center justify-center">
+          <div
+            className={themeManagerCn(
+              "flex items-center justify-center",
+              locatorVariants({ backgroundColor }),
+            )}
+          >
             <p className="pt-4 text-2xl">No results found for this area</p>
           </div>
         )}
