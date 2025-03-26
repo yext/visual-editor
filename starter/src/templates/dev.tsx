8cb92efe78c749bba34106c395aa6c7d927afd27
Fondryext <160865254+Fondryext@users.noreply.github.com>
2025-03-26 10:53:29 -0400

    feat: add basic result cards (#277)
    This PR adds basic result cards, still in need of styling. It also
    adjusts how the SearchHeadlessProvider is created, so that the
    call to provideHeadless is not inside of a component. This may need
    adjusting.
    
    J=WAT-4699
    TEST=manual
    
    Results are populated, albeit unstyled. No rerendering issues

--- starter/src/templates/dev.tsx
+++ starter/src/templates/dev.tsx
@@ -20,6 +20,13 @@ import { buildSchema } from "../utils/buildSchema.ts";
 import tailwindConfig from "../../tailwind.config";
 import { devTemplateStream } from "../dev.config";
 import React from "react";
+import {
+  CloudChoice,
+  CloudRegion,
+  Environment,
+  provideHeadless,
+  SearchHeadlessProvider,
+} from "@yext/search-headless-react";
 
 export const config = {
   name: "dev-location",
@@ -106,6 +113,18 @@ const Dev: Template<TemplateRenderProps> = (props) => {
   const { document } = props;
   const entityFields = devTemplateStream.stream.schema
     .fields as unknown as YextSchemaField[];
+  const config = {
+    apiKey: "",
+    experienceKey: "jacob-test",
+    locale: "en",
+    experienceVersion: "STAGING",
+    verticalKey: "locations",
+    businessId: document.businessId,
+    cloudRegion: CloudRegion.US,
+    cloudChoice: CloudChoice.GLOBAL_MULTI,
+    environment: Environment.PROD,
+  };
+  const searcher = provideHeadless(config);
 
   return (
     <div>
@@ -120,19 +139,21 @@ const Dev: Template<TemplateRenderProps> = (props) => {
         </button>
       </div>
       <div>
-        <VisualEditorProvider
-          templateProps={props}
-          entityFields={entityFields}
-          tailwindConfig={tailwindConfig}
-        >
-          <Editor
-            document={document}
-            componentRegistry={componentRegistry}
-            themeConfig={themeConfig}
-            localDev={true}
-            forceThemeMode={themeMode}
-          />
-        </VisualEditorProvider>
+        <SearchHeadlessProvider searcher={searcher}>
+          <VisualEditorProvider
+            templateProps={props}
+            entityFields={entityFields}
+            tailwindConfig={tailwindConfig}
+          >
+            <Editor
+              document={document}
+              componentRegistry={componentRegistry}
+              themeConfig={themeConfig}
+              localDev={true}
+              forceThemeMode={themeMode}
+            />
+          </VisualEditorProvider>
+        </SearchHeadlessProvider>
       </div>
     </div>
   );
