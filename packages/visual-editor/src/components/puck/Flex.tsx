190b6e3f2fd5976e286b671a5f86fd06f518a4c4
Brian Stephan <bstephan@yext.com>
2025-03-28 13:46:51 -0600

    feat: flex component updates

--- packages/visual-editor/src/components/puck/Flex.tsx
+++ packages/visual-editor/src/components/puck/Flex.tsx
@@ -71,19 +71,31 @@ const FlexContainer = React.forwardRef<HTMLDivElement, FlexProps>(
 FlexContainer.displayName = "Flex";
 
 const flexContainerFields: Fields<FlexProps> = {
-  direction: BasicSelector("Direction", [
-    { value: "row", label: "Horizontal" },
-    { value: "column", label: "Vertical" },
-  ]),
-  justifyContent: BasicSelector("Justify Content", [
-    { value: "start", label: "Start" },
-    { value: "center", label: "Center" },
-    { value: "end", label: "End" },
-  ]),
-  wrap: BasicSelector("Wrap", [
-    { value: "nowrap", label: "No Wrap" },
-    { value: "wrap", label: "Wrap" },
-  ]),
+  direction: {
+    label: "Direction",
+    type: "radio",
+    options: [
+      { label: "Horizontal", value: "row" },
+      { label: "Vertical", value: "column" },
+    ],
+  },
+  justifyContent: {
+    label: "Justify Content",
+    type: "radio",
+    options: [
+      { label: "Start", value: "start" },
+      { label: "Center", value: "center" },
+      { label: "End", value: "end" },
+    ],
+  },
+  wrap: {
+    label: "Wrap",
+    type: "radio",
+    options: [
+      { label: "No Wrap", value: "nowrap" },
+      { label: "Wrap", value: "wrap" },
+    ],
+  },
   ...layoutFields,
 };
 
@@ -91,7 +103,7 @@ const FlexContainerComponent: ComponentConfig<FlexProps> = {
   label: "Flex",
   fields: flexContainerFields,
   defaultProps: {
-    direction: "column",
+    direction: "row",
     justifyContent: "start",
     wrap: "nowrap",
     gap: "0",
@@ -99,7 +111,7 @@ const FlexContainerComponent: ComponentConfig<FlexProps> = {
     horizontalPadding: "0",
     backgroundColor: backgroundColors.background1.value,
   },
-  resolveFields: (data, params) => {
+  resolveFields: (data: { props: FlexProps }, params: { parent: any }) => {
     // If the Flex has a parent component, the defaultProps should
     // be adjusted and maxContentWidth should not be a field.
     if (params.parent) {
@@ -135,7 +147,7 @@ const FlexContainerComponent: ComponentConfig<FlexProps> = {
     className,
     backgroundColor,
     maxContentWidth,
-  }) => (
+  }: FlexProps) => (
     <FlexContainer
       direction={direction}
       justifyContent={justifyContent}
