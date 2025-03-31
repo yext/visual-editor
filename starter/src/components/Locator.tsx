26ba4df61c0e4eb9f0e2aeec5721c91ca9afe160
Kyle Gerner <49618240+k-gerner@users.noreply.github.com>
2025-03-31 11:23:48 -0400

    feat: integrate mapbox map component within locator component (#296)
    I have added the `MapboxMap` from `search-ui-react` to the Locator
    component.
    Currently, the only property of the map that is configurable is the
    "style"
    of the map (e.g. street, dark mode, day navigation, etc).
    The map will attempt to find the user's location, and use that as the
    first search. The user can either drag/zoom around the map and click
    "Search this area" to run a search, or they can use the filter search
    bar.
    
    The display will be the following:
    - for small screen sizes, the map component will be hidden
    - for medium and larger screen sizes, the filter search + results will
    occupy the left 1/3 of the screen, while the map will occupy the
    remaining
    right 2/3.
    
    To see it in action (prior to card styling changes), see this video:
    
    https://drive.google.com/file/d/1hvAdD6mtwnOLRXfU_0cN0jokzBqnBfOx/view?usp=drive_link
    
    
    J=[WAT-4700](https://yexttest.atlassian.net/browse/WAT-4700?atlOrigin=eyJpIjoiYTIzY2NjNDllYTdhNDE4MWJlNzU3ZjZlZGFmZmIzYTciLCJwIjoiaiJ9)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Jacob Fondriest <jfondriest@yext.com>

--- starter/src/components/Locator.tsx
+++ starter/src/components/Locator.tsx
@@ -4,6 +4,9 @@ import {
   CardProps,
   Coordinate,
   FilterSearch,
+  getUserLocation,
+  MapboxMap,
+  OnDragHandler,
   OnSelectParams,
   VerticalResults,
 } from "@yext/search-ui-react";
@@ -16,6 +19,7 @@ import {
 import * as React from "react";
 import { cva, VariantProps } from "class-variance-authority";
 import { BasicSelector, Button, themeManagerCn } from "@yext/visual-editor";
+import { LngLat, LngLatBounds } from "mapbox-gl";
 import { normalizeSlug } from "@yext/visual-editor";
 import { useEffect, useState } from "react";
 import { HoursStatus, Link } from "@yext/pages-components";
@@ -23,6 +27,8 @@ import { Address, Hours } from "../types/autogen.ts";
 
 const DEFAULT_FIELD = "builtin.location";
 const DEFAULT_ENTITY_TYPE = "location";
+const DEFAULT_MAP_CENTER = [-74.005371, 40.741611]; // New York City
+const DEFAULT_RADIUS_METERS = 40233.6; // 25 miles
 
 const locatorVariants = cva("", {
   variants: {
@@ -40,7 +46,9 @@ const locatorVariants = cva("", {
   },
 });
 
-type LocatorProps = VariantProps<typeof locatorVariants>;
+interface LocatorProps extends VariantProps<typeof locatorVariants> {
+  mapStyle?: string;
+}
 
 const locatorFields: Fields<LocatorProps> = {
   backgroundColor: BasicSelector("Background Color", [
@@ -50,6 +58,23 @@ const locatorFields: Fields<LocatorProps> = {
     { label: "Accent", value: "accent" },
     { label: "Text", value: "text" },
     { label: "Background", value: "background" },
+  ]),
+  mapStyle: BasicSelector("Map Style", [
+    { label: "Default", value: "mapbox://styles/mapbox/streets-v12" },
+    {
+      label: "Satellite",
+      value: "mapbox://styles/mapbox/satellite-streets-v12",
+    },
+    { label: "Light", value: "mapbox://styles/mapbox/light-v11" },
+    { label: "Dark", value: "mapbox://styles/mapbox/dark-v11" },
+    {
+      label: "Navigation (Day)",
+      value: "mapbox://styles/mapbox/navigation-day-v1",
+    },
+    {
+      label: "Navigation (Night)",
+      value: "mapbox://styles/mapbox/navigation-night-v1",
+    },
   ]),
 };
 
@@ -62,10 +87,43 @@ const LocatorComponent: ComponentConfig<LocatorProps> = {
 type SearchState = "not started" | "loading" | "complete";
 
 const Locator: React.FC<LocatorProps> = (props) => {
-  const { backgroundColor } = props;
+  const { backgroundColor, mapStyle } = props;
   const resultCount = useSearchState(
     (state) => state.vertical.resultsCount || 0,
   );
+
+  const [showSearchAreaButton, setShowSearchAreaButton] = React.useState(false);
+  const [mapCenter, setMapCenter] = React.useState<LngLat | undefined>();
+  const [mapBounds, setMapBounds] = React.useState<LngLatBounds | undefined>();
+
+  const handleDrag: OnDragHandler = (center: LngLat, bounds: LngLatBounds) => {
+    setMapCenter(center);
+    setMapBounds(bounds);
+    setShowSearchAreaButton(true);
+  };
+
+  const handleSearchAreaClick = () => {
+    if (mapCenter && mapBounds) {
+      const locationFilter: SelectableStaticFilter = {
+        selected: true,
+        displayName: "Current map area",
+        filter: {
+          kind: "fieldValue",
+          fieldId: "builtin.location",
+          value: {
+            lat: mapCenter.lat,
+            lng: mapCenter.lng,
+            radius: mapBounds.getNorthEast().distanceTo(mapCenter),
+          },
+          matcher: Matcher.Near,
+        },
+      };
+      searchActions.setStaticFilters([locationFilter]);
+      searchActions.executeVerticalQuery();
+      setSearchState("loading");
+      setShowSearchAreaButton(false);
+    }
+  };
 
   const searchActions = useSearchActions();
   const handleFilterSelect = (params: OnSelectParams) => {
@@ -84,7 +142,55 @@ const Locator: React.FC<LocatorProps> = (props) => {
     setSearchState("loading");
   };
 
+  const [userLocation, setUserLocation] =
+    React.useState<number[]>(DEFAULT_MAP_CENTER);
+  React.useEffect(() => {
+    getUserLocation()
+      .then((location) => {
+        setUserLocation([location.coords.longitude, location.coords.latitude]);
+        searchActions.setStaticFilters([
+          {
+            selected: true,
+            displayName: "Current Location",
+            filter: {
+              kind: "fieldValue",
+              fieldId: "builtin.location",
+              value: {
+                lat: location.coords.latitude,
+                lng: location.coords.longitude,
+                radius: DEFAULT_RADIUS_METERS,
+              },
+              matcher: Matcher.Near,
+            },
+          },
+        ]);
+      })
+      .catch(() => {
+        searchActions.setStaticFilters([
+          {
+            selected: true,
+            displayName: "New York City, New York, NY",
+            filter: {
+              kind: "fieldValue",
+              fieldId: "builtin.location",
+              value: {
+                lat: DEFAULT_MAP_CENTER[1],
+                lng: DEFAULT_MAP_CENTER[0],
+                radius: DEFAULT_RADIUS_METERS,
+              },
+              matcher: Matcher.Near,
+            },
+          },
+        ]);
+      })
+      .then(() => {
+        searchActions.executeVerticalQuery();
+        setSearchState("loading");
+      });
+  }, []);
+
   const searchLoading = useSearchState((state) => state.searchStatus.isLoading);
+
   const [searchState, setSearchState] = useState<SearchState>("not started");
 
   useEffect(() => {
@@ -93,28 +199,93 @@ const Locator: React.FC<LocatorProps> = (props) => {
     }
   }, [searchLoading]);
 
+  const mapProps: MapProps = {
+    ...(userLocation && { centerCoords: userLocation }),
+    ...(mapStyle && { mapStyle }),
+    onDragHandler: handleDrag,
+  };
+
   return (
     <>
-      <FilterSearch
-        searchFields={[
-          { fieldApiName: DEFAULT_FIELD, entityType: DEFAULT_ENTITY_TYPE },
-        ]}
-        onSelect={(params) => handleFilterSelect(params)}
-      />
-      <div>
-        {resultCount > 0 && <VerticalResults CardComponent={LocationCard} />}
-        {resultCount === 0 && searchState === "complete" && (
-          <div
-            className={themeManagerCn(
-              "flex items-center justify-center",
-              locatorVariants({ backgroundColor }),
+      <div className="flex w-full h-full aspect-[1/2] md:aspect-[3/2]">
+        {/* Left Section: FilterSearch + Results. Full width for small screens */}
+        <div className="w-full md:w-1/3 p-4 h-full flex flex-col">
+          <FilterSearch
+            searchFields={[
+              { fieldApiName: DEFAULT_FIELD, entityType: DEFAULT_ENTITY_TYPE },
+            ]}
+            onSelect={(params) => handleFilterSelect(params)}
+          />
+          <div id="innerDiv" className="flex-grow overflow-y-auto">
+            {resultCount > 0 && (
+              <VerticalResults CardComponent={LocationCard} />
             )}
-          >
-            <p className="pt-4 text-2xl">No results found for this area</p>
+            {resultCount === 0 && searchState === "complete" && (
+              <div
+                className={themeManagerCn(
+                  "flex items-center justify-center",
+                  locatorVariants({ backgroundColor }),
+                )}
+              >
+                <p className="pt-4 text-2xl">No results found for this area</p>
+              </div>
+            )}
           </div>
-        )}
+        </div>
+
+        {/* Right Section: Map. Hidden for small screens */}
+        <div className="w-2/3 md:flex hidden">
+          <div className="relative w-full h-full">
+            <Map {...mapProps} />
+            {showSearchAreaButton && (
+              <div className="absolute bottom-10 left-0 right-0 flex justify-center">
+                <button
+                  onClick={handleSearchAreaClick}
+                  className="rounded-2xl border bg-white py-2 px-4 shadow-xl"
+                >
+                  <p>Search This Area</p>
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </>
+  );
+};
+
+interface MapProps {
+  mapStyle?: string;
+  centerCoords?: number[];
+  onDragHandler?: OnDragHandler;
+}
+
+const Map: React.FC<MapProps> = ({ mapStyle, centerCoords, onDragHandler }) => {
+  const iframe = document.getElementById("preview-frame") as HTMLIFrameElement;
+  const mapboxApiKey = "";
+  if (iframe?.contentDocument && !iframe.contentWindow?.mapboxgl) {
+    // We are in an iframe, and mapboxgl is not loaded in yet
+    return (
+      <div className="flex items-center justify-center w-full h-full">
+        <div className="border border-gray-300 rounded-lg p-6 bg-white shadow-md">
+          <span className="text-gray-700 text-lg font-medium">
+            Loading Map...
+          </span>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <MapboxMap
+      mapboxAccessToken={mapboxApiKey || ""}
+      mapboxOptions={{
+        center: centerCoords ?? DEFAULT_MAP_CENTER,
+        ...(mapStyle ? { style: mapStyle } : {}),
+      }}
+      onDrag={onDragHandler}
+      iframeWindow={iframe?.contentWindow ?? undefined}
+      allowUpdates={!!iframe?.contentDocument}
+    />
   );
 };
 
