2c136816d8f76b4bf55abe6c0fd8aefba73b5833
Ben Life <blife@yext.com>
2025-04-04 14:17:50 -0400

    update theme defaults

--- starter/theme.config.ts
+++ starter/theme.config.ts
@@ -65,7 +65,7 @@ export const themeConfig: ThemeConfig = {
         type: "select",
         plugin: "fontSize",
         options: ThemeOptions.FONT_SIZE,
-        default: "24px",
+        default: "48px",
       },
       fontWeight: {
         label: "Font Weight",
@@ -91,7 +91,7 @@ export const themeConfig: ThemeConfig = {
         type: "select",
         plugin: "fontSize",
         options: ThemeOptions.FONT_SIZE,
-        default: "24px",
+        default: "36px",
       },
       fontWeight: {
         label: "Font Weight",
@@ -117,7 +117,7 @@ export const themeConfig: ThemeConfig = {
         type: "select",
         plugin: "fontSize",
         options: ThemeOptions.FONT_SIZE,
-        default: "24px",
+        default: "30px",
       },
       fontWeight: {
         label: "Font Weight",
@@ -169,7 +169,7 @@ export const themeConfig: ThemeConfig = {
         type: "select",
         plugin: "fontSize",
         options: ThemeOptions.FONT_SIZE,
-        default: "24px",
+        default: "20px",
       },
       fontWeight: {
         label: "Font Weight",
@@ -195,7 +195,7 @@ export const themeConfig: ThemeConfig = {
         type: "select",
         plugin: "fontSize",
         options: ThemeOptions.FONT_SIZE,
-        default: "24px",
+        default: "18px",
       },
       fontWeight: {
         label: "Font Weight",
