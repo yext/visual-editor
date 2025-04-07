911d70cb9f60ae95ad9b620a403db1427930c400
Alexis Sanehisa <asanehisa@yext.com>
2025-04-07 11:54:11 -0400

    empty line

--- packages/visual-editor/src/components/puck/FAQs.tsx
+++ packages/visual-editor/src/components/puck/FAQs.tsx
@@ -26,6 +26,7 @@ const DEFAULT_FAQ = {
   answer:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
 };
+
 export interface FAQsSectionProps {
   styles: {
     backgroundColor?: BackgroundStyle;
