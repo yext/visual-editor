8a494295877f420aca056711d6eb8d0803746d98
Alexis Sanehisa <asanehisa@yext.com>
2025-04-07 11:47:15 -0400

    add accordion actions

--- packages/visual-editor/tailwind.config.ts
+++ packages/visual-editor/tailwind.config.ts
@@ -49,6 +49,20 @@ export default {
           xl: "5rem",
         },
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
   plugins: [],
