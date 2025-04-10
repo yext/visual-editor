fd2a53f2c0cd99c314fa1a4579df443c2ec8304d
Alexis Sanehisa <asanehisa@yext.com>
2025-04-10 19:11:08 -0400

    trim

--- packages/visual-editor/src/components/puck/MapboxStaticMap.tsx
+++ packages/visual-editor/src/components/puck/MapboxStaticMap.tsx
@@ -31,10 +31,9 @@ const mapboxFields: Fields<MapboxStaticProps> = {
 const getPrimaryColor = (document: any) => {
   if (document?.__?.theme) {
     return (
-      JSON.parse(document?.__?.theme)?.["--colors-palette-primary"].replace(
-        "#",
-        ""
-      ) ?? "000000"
+      JSON.parse(document?.__?.theme)
+        ?.["--colors-palette-primary"].trim()
+        .replace("#", "") ?? "000000"
     );
   } else {
     const iframe = window.document.querySelector("iframe");
