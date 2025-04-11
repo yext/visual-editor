3d7aec0711c553fe79e44e00e8cb85e42b127eda
Jacob Wartofsky <jwartofsky@yext.com>
2025-04-11 16:37:19 -0400

    swap to object-cover

--- packages/visual-editor/src/components/puck/MapboxStaticMap.tsx
+++ packages/visual-editor/src/components/puck/MapboxStaticMap.tsx
@@ -8,14 +8,11 @@ import {
 } from "../../index.js";
 import { ComponentConfig, Fields } from "@measured/puck";
 
-const WIDTH_MIN = 300;
-const WIDTH_MAX = 2048;
-const HEIGHT_MIN = 300;
-const HEIGHT_MAX = 2048;
-
 type MapboxStaticProps = {
   apiKey: string;
   coordinate: YextEntityField<Coordinate>;
+  width?: number;
+  height?: number;
   zoom?: number;
   mapStyle?: string;
 };
@@ -54,28 +51,12 @@ const getPrimaryColor = (document: any) => {
 const MapboxStaticMap = ({
   apiKey,
   coordinate: coordinateField,
+  width = 1024,
+  height = 300,
   zoom = 14,
   mapStyle = "light-v11",
 }: MapboxStaticProps) => {
   const document = useDocument<any>();
-
-  const ref = React.useRef<HTMLImageElement | null>(null);
-  const [size, setSize] = React.useState({ width: 1024, height: 300 });
-
-  React.useEffect(() => {
-    const observer = new ResizeObserver((entries) => {
-      for (const entry of entries) {
-        const { width, height } = entry.contentRect;
-        setSize({ width, height });
-      }
-    });
-
-    if (ref.current?.parentElement) {
-      observer.observe(ref.current?.parentElement);
-    }
-
-    return () => observer.disconnect();
-  }, []);
 
   const coordinate = resolveYextEntityField<Coordinate>(
     document,
@@ -92,14 +73,10 @@ const MapboxStaticMap = ({
 
   const marker = `pin-l+${getPrimaryColor(document)}(${coordinate.longitude},${coordinate.latitude})`;
 
-  const heightClamped = Math.min(Math.max(size.height, HEIGHT_MIN), HEIGHT_MAX);
-  const widthClamped = Math.min(Math.max(size.width, WIDTH_MIN), WIDTH_MAX);
-
   return (
     <img
-      ref={ref}
-      className="components w-full"
-      src={`https://api.mapbox.com/styles/v1/mapbox/${mapStyle}/static/${marker}/${coordinate.longitude},${coordinate.latitude},${zoom}/${widthClamped}x${heightClamped}?access_token=${apiKey}`}
+      className="components w-full h-full object-cover"
+      src={`https://api.mapbox.com/styles/v1/mapbox/${mapStyle}/static/${marker}/${coordinate.longitude},${coordinate.latitude},${zoom}/${width}x${height}?access_token=${apiKey}`}
     />
   );
 };
