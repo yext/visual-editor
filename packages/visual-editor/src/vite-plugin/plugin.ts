0afb7f7a44cfcd42d5baf2823757ed8beba7ebba
Ben Life <blife@yext.com>
2025-05-05 16:08:15 -0400

    remove space

--- packages/visual-editor/src/vite-plugin/plugin.ts
+++ packages/visual-editor/src/vite-plugin/plugin.ts
@@ -63,7 +63,6 @@ export const yextVisualEditorPlugin = (): Plugin => {
         return;
       }
       generateFiles();
-
       server.httpServer?.on("close", () => {
         cleanupFiles();
       });
