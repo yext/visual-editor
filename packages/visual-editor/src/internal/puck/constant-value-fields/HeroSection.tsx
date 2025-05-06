eb287f9782cb01e3fef1cf95ee114348b5ed4330
Alexis Sanehisa <asanehisa@yext.com>
2025-05-06 09:49:50 -0400

    move code

--- packages/visual-editor/src/internal/puck/constant-value-fields/HeroSection.tsx
+++ packages/visual-editor/src/internal/puck/constant-value-fields/HeroSection.tsx
@@ -1,49 +1,6 @@
 import { Field } from "@measured/puck";
 import { CTA, ImageType } from "@yext/pages-components";
-
-const ctaFields: Field<CTA> = {
-  type: "object",
-  objectFields: {
-    label: {
-      label: "Label",
-      type: "text",
-    },
-    link: {
-      label: "Link",
-      type: "text",
-    },
-    linkType: {
-      label: "Link Type",
-      type: "select",
-      options: [
-        {
-          label: "URL",
-          value: "URL",
-        },
-        {
-          label: "Email",
-          value: "EMAIL",
-        },
-        {
-          label: "Phone",
-          value: "PHONE",
-        },
-        {
-          label: "Click to Website",
-          value: "CLICK_TO_WEBSITE",
-        },
-        {
-          label: "Driving Directions",
-          value: "DRIVING_DIRECTIONS",
-        },
-        {
-          label: "Other",
-          value: "OTHER",
-        },
-      ],
-    },
-  },
-};
+import { ctaFields } from "./CallToAction.tsx";
 
 export const HERO_CONSTANT_CONFIG: Field<{
   image: ImageType;
