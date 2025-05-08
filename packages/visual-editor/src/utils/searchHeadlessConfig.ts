bdf3ffd6cd076ae559a13f33ea03e1d5faf6af3a
Kyle Gerner <kgerner@yext.com>
2025-05-08 15:26:09 -0400

    feat: create locator template

--- packages/visual-editor/src/utils/searchHeadlessConfig.ts
+++ packages/visual-editor/src/utils/searchHeadlessConfig.ts
@@ -5,6 +5,8 @@ import {
   HeadlessConfig,
 } from "@yext/search-headless-react";
 
+const EXPERIENCE_VERSION = "PRODUCTION";
+
 /**
  * Builds the search headless config for the template. Returns undefined if the config is not valid.
  * @param document the entity document
@@ -49,13 +51,59 @@ export const createSearchHeadlessConfig = (document: any) => {
     apiKey: searchApiKey,
     experienceKey: experienceKey,
     locale: document.locale,
-    experienceVersion: "PRODUCTION",
+    experienceVersion: EXPERIENCE_VERSION,
     verticalKey: "locations",
     cloudRegion: cloudRegion,
     cloudChoice: cloudChoice,
     environment: environment,
   };
   return headlessConfig;
+};
+
+/**
+ * Builds the search analytics config for the template. Returns undefined if the config is not valid.
+ * @param document the entity document
+ */
+export const createSearchAnalyticsConfig = (document: any) => {
+  const warnings = [];
+  const businessId = document?.businessId;
+  if (!businessId) {
+    warnings.push("Missing businessId! Unable to set up locator analytics.");
+  }
+  const environment = document?._env?.YEXT_ENVIRONMENT;
+  if (!environment || !isValidEnvironment(environment)) {
+    warnings.push(
+      "Invalid or missing YEXT_ENVIRONMENT! Unable to set up locator analytics."
+    );
+  }
+  const cloudRegion = document?._env?.YEXT_CLOUD_REGION;
+  if (!cloudRegion || !isValidCloudRegion(cloudRegion)) {
+    warnings.push(
+      "Invalid or missing YEXT_CLOUD_REGION! Unable to set up locator analytics."
+    );
+  }
+  if (warnings.length > 0) {
+    warnings.forEach((msg) => console.warn(msg));
+    return;
+  }
+
+  // from @yext/analytics EnvironmentEnum
+  const analyticsEnvironment =
+    environment === Environment.SANDBOX ? "SANDBOX" : "PRODUCTION";
+  // from @yext/analytics RegionEnum
+  const analyticsRegion = cloudRegion.toUpperCase();
+  // experienceKey will eventually be on the entity document, waiting on Spruce
+  // https://yext.slack.com/archives/C06A06BCUUF/p1745963801890889
+  const experienceKey = "jacob-test";
+  // from @yext/analytics SearchAnalyticsConfig
+  const analyticsConfig = {
+    businessId: businessId,
+    experienceKey: experienceKey,
+    experienceVersion: EXPERIENCE_VERSION,
+    region: analyticsRegion,
+    env: analyticsEnvironment,
+  };
+  return analyticsConfig;
 };
 
 const isValidCloudRegion = (value: any): value is CloudRegion => {
