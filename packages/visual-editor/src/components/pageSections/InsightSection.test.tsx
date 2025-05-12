1119223c8958717f38b089ce56737b3c504c9ad4
colton-demetriou <cdemetriou@yext.com>
2025-05-12 15:24:39 -0400

    add test

--- packages/visual-editor/src/components/pageSections/InsightSection.test.tsx
+++ packages/visual-editor/src/components/pageSections/InsightSection.test.tsx
@@ -37,4 +37,68 @@ describe.each(viewports)("InsightSection $name", ({ width, height }) => {
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
+
+  it("should pass wcag with insights", async () => {
+    const { container } = reactRender(
+      <VisualEditorProvider
+        templateProps={{
+          document: {
+            c_insightsSection: {
+              insights: [
+                {
+                  image: {
+                    url: "https://placehold.co/600x400",
+                    width: 600,
+                    height: 400,
+                  },
+                  name: "Insight 1",
+                  category: "Category 1",
+                  publishTime: "2025-05-12T13:00:00Z",
+                  description: { html: "<strong>Test</strong> RTF" },
+                  cta: {
+                    link: "https://yext.com",
+                    label: "Learn More",
+                    linkType: "URL",
+                  },
+                },
+                {
+                  name: "Insight 2",
+                  category: "Category 2",
+                  publishTime: "invalid date",
+                  description: "This is normal text",
+                },
+              ],
+            },
+          },
+        }}
+      >
+        <Render
+          config={puckConfig}
+          data={{
+            content: [
+              {
+                type: "InsightSection",
+                props: {
+                  id: "abc",
+                  ...InsightSection.defaultProps,
+                  data: {
+                    ...InsightSection.defaultProps!.data,
+                    insights: {
+                      field: "c_insightsSection",
+                      constantValue: {},
+                    },
+                  },
+                },
+              },
+            ],
+          }}
+        />
+      </VisualEditorProvider>
+    );
+
+    await page.viewport(width, height);
+    await page.screenshot();
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
 });
