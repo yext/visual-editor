156506b2a8b6b9d3e8a13a06e504a0aeae6aa98e
rkeerthient <rkeerthi@yext.com>
2025-05-30 02:08:09 +0530

    fix: removed italic from address tag

--- packages/visual-editor/src/components/pageSections/FINS/FINS_HeroSection.tsx
+++ packages/visual-editor/src/components/pageSections/FINS/FINS_HeroSection.tsx
@@ -311,7 +311,6 @@ const HeroSectionWrapper = ({
                   constantValueEnabled={address.constantValueEnabled}
                 >
                   <Address
-                    className="not-italic"
                     address={resolvedAddress}
                     lines={[
                       ["line1"],
