ce228cae8aadce0b67aed55c04531b936b8ceedf
Ben Life <blife@yext.com>
2025-06-02 16:42:40 -0400

    footer wip

--- packages/visual-editor/src/components/Footer.tsx
+++ packages/visual-editor/src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { CTA as CTAType } from "@yext/pages-components";
+import { ComplexImageType, CTA as CTAType } from "@yext/pages-components";
 import { ComponentConfig, Fields, WithId, WithPuckProps } from "@measured/puck";
 import {
   Body,
@@ -10,6 +10,7 @@ import {
   backgroundColors,
   PageSection,
   YextField,
+  Image,
 } from "@yext/visual-editor";
 import {
   FaFacebook,
@@ -28,8 +29,14 @@ type socialLink = {
   prefix?: string;
 };
 
+type FooterLinkSection = {
+  label: string;
+  links: CTAType[];
+};
+
 type FooterProps = {
   backgroundColor?: BackgroundStyle;
+  logoWidth: number;
 };
 
 const footerFields: Fields<FooterProps> = {
@@ -38,13 +45,18 @@ const footerFields: Fields<FooterProps> = {
     hasSearch: true,
     options: "BACKGROUND_COLOR",
   }),
+  logoWidth: YextField("Logo Width", {
+    type: "number",
+    min: 0,
+  }),
 };
 
 const Footer: ComponentConfig<FooterProps> = {
   label: "Footer",
   fields: footerFields,
   defaultProps: {
-    backgroundColor: backgroundColors.background1.value,
+    backgroundColor: backgroundColors.background6.value,
+    logoWidth: 80,
   },
   inline: true,
   render: (props) => <FooterComponent {...props} />,
@@ -53,12 +65,30 @@ const Footer: ComponentConfig<FooterProps> = {
 const FooterComponent: React.FC<WithId<WithPuckProps<FooterProps>>> = (
   props
 ) => {
-  const document = useDocument<any>();
+  const document: {
+    _site?: {
+      footer?: {
+        linkSections?: FooterLinkSection[];
+        images?: ComplexImageType[];
+      };
+      copyrightMessage?: string;
+      logo?: ComplexImageType;
+      facebookPageUrl?: string;
+      instagramHandle?: string;
+      youTubeChannelUrl?: string;
+      linkedInUrl?: string;
+      twitterHandle?: string;
+      pinterestUrl?: string;
+      tikTokUrl?: string;
+    };
+  } = useDocument();
   const { backgroundColor = backgroundColors.background1.value, puck } = props;
 
-  const links = document?._site?.footer?.links ?? [];
+  const linkSections = document?._site?.footer?.linkSections;
   const copyrightMessage = document?._site?.copyrightMessage;
-  const socialLinks: socialLink[] = [
+  const logo = document?._site?.logo;
+  const images = document?._site?.footer?.images;
+  const socialLinks = [
     {
       name: "facebook",
       link: document?._site?.facebookPageUrl,
@@ -96,28 +126,65 @@ const FooterComponent: React.FC<WithId<WithPuckProps<FooterProps>>> = (
       link: document?._site?.tikTokUrl,
       label: <FaTiktok className="w-5 h-5 mr-4" />,
     },
-  ].filter((link) => link.link);
+  ].filter((link) => link.link) as socialLink[];
 
   return (
     <PageSection
       background={backgroundColor}
       className="flex flex-col"
-      outerClassName="mt-auto"
+      outerClassName="mt-auto p-20"
       as="footer"
       ref={puck.dragRef}
     >
-      <div className="flex flex-col sm:flex-row justify-between w-full items-center text-body-fontSize font-body-fontFamily">
-        {links && (
+      <div className="flex flex-col sm:flex-row justify-between w-full items-center text-body-fontSize font-body-fontFamily mb-10">
+        <div className="flex flex-col justify-between items-start gap-8">
+          {logo && (
+            <EntityField
+              displayName="Business Logo"
+              fieldId={"site.businessLogo"}
+            >
+              <div
+                className="flex mr-2"
+                style={{ width: `${props.logoWidth}px` }}
+              >
+                <Image
+                  image={logo.image}
+                  layout="auto"
+                  aspectRatio={logo.image.width / logo.image.height}
+                />
+              </div>
+            </EntityField>
+          )}
+          {socialLinks && (
+            <EntityField
+              displayName="Footer Social Icons"
+              fieldId={"site.footer"}
+            >
+              <FooterSocialIcons socialLinks={socialLinks} />
+            </EntityField>
+          )}
+          {images && images.length > 0 && (
+            <EntityField
+              displayName="Footer Images"
+              fieldId="site.footer.images"
+            >
+              <div className="flex justify-between items-center gap-8">
+                {images.map((image, idx) => (
+                  <Image
+                    key={"footerimage" + idx}
+                    image={image}
+                    layout="auto"
+                    aspectRatio={1}
+                    className="rounded-full"
+                  />
+                ))}
+              </div>
+            </EntityField>
+          )}
+        </div>
+        {linkSections && linkSections.length > 0 && (
           <EntityField displayName="Footer Links" fieldId={"site.footer.links"}>
-            <FooterLinks links={links} />
-          </EntityField>
-        )}
-        {socialLinks && (
-          <EntityField
-            displayName="Footer Social Icons"
-            fieldId={"site.footer"}
-          >
-            <FooterSocialIcons socialLinks={socialLinks} />
+            <FooterLinks sections={linkSections} />
           </EntityField>
         )}
       </div>
@@ -127,7 +194,7 @@ const FooterComponent: React.FC<WithId<WithPuckProps<FooterProps>>> = (
             displayName="Copyright Text"
             fieldId="site.copyrightMessage"
           >
-            <Body>{copyrightMessage}</Body>
+            <Body variant={"sm"}>{copyrightMessage}</Body>
           </EntityField>
         </div>
       )}
@@ -135,31 +202,43 @@ const FooterComponent: React.FC<WithId<WithPuckProps<FooterProps>>> = (
   );
 };
 
-const FooterLinks = (props: { links: CTAType[] }) => {
+const FooterLinks = (props: { sections?: FooterLinkSection[] }) => {
+  const { sections } = props;
+
+  if (!sections?.length) {
+    return;
+  }
+
   return (
-    <ul className="flex flex-col sm:flex-row items-center pb-4">
-      {props.links
-        .filter((item) => !!item?.link)
-        .map((item, idx) => (
-          <li key={item.link}>
-            <CTA
-              link={item.link}
-              label={item.label}
-              linkType={item.linkType}
-              eventName={`footerlink${idx}`}
-              variant="link"
-              alwaysHideCaret={true}
-              className="sm:pr-8"
-            />
-          </li>
-        ))}
-    </ul>
+    <EntityField displayName="Footer Links" fieldId="site.footer.linkSections">
+      {sections.map((section, i) => {
+        return (
+          <div key={"footersection" + i}>
+            <Body>{section.label}</Body>
+            {section.links.map((item, j) => {
+              return (
+                <CTA
+                  key={`footerlink-${i}-${j}`}
+                  link={item.link}
+                  label={item.label}
+                  linkType={item.linkType}
+                  eventName={`footerlink${i}-${j}`}
+                  variant="link"
+                  alwaysHideCaret={true}
+                  className="sm:pr-8"
+                />
+              );
+            })}
+          </div>
+        );
+      })}
+    </EntityField>
   );
 };
 
 const FooterSocialIcons = ({ socialLinks }: { socialLinks: socialLink[] }) => {
   return (
-    <div className="flex flex-row items-center justify-center sm:justify-end pb-4">
+    <div className="flex flex-row items-center justify-center sm:justify-end">
       {socialLinks.map((socialLink: socialLink, idx: number) =>
         socialLink.link ? (
           <CTA
