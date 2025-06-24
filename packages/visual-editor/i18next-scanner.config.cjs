3bacaacc545815e4fc7bfc14d869e02b667f4e77
Alexis Sanehisa <asanehisa@yext.com>
2025-06-24 10:25:38 -0400

    yay

--- packages/visual-editor/i18next-scanner.config.cjs
+++ packages/visual-editor/i18next-scanner.config.cjs
@@ -60,5 +60,6 @@ module.exports = {
       prefix: "{{",
       suffix: "}}",
     },
+    contextSeparator: "_",
   },
 };
--- packages/visual-editor/src/components/pageSections/PromoSection.tsx
+++ packages/visual-editor/src/components/pageSections/PromoSection.tsx
@@ -77,8 +77,18 @@ const promoSectionFields: Fields<PromoSectionProps> = {
         {
           type: "radio",
           options: [
-            { label: msg("fields.options.left", "Left"), value: "left" },
-            { label: msg("fields.options.right", "Right"), value: "right" },
+            {
+              label: msg("fields.options.left", "Left", {
+                context: "direction",
+              }),
+              value: "left",
+            },
+            {
+              label: msg("fields.options.right", "Right", {
+                context: "direction",
+              }),
+              value: "right",
+            },
           ],
         }
       ),
