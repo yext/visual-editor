95047ca51e2b2facd0abfcb776c2c61446e9823b
Kyle Gerner <kgerner@yext.com>
2025-06-27 15:35:48 -0400

    add bulk of component (content, review data, etc)

--- starter/src/components/ReviewsSectionDemo.tsx
+++ starter/src/components/ReviewsSectionDemo.tsx
@@ -1,60 +1,86 @@
 import { useTranslation } from "react-i18next";
-import { FaStarHalfAlt, FaStar, FaRegStar } from "react-icons/fa";
+import { FaStarHalfAlt, FaStarHalf, FaStar, FaRegStar } from "react-icons/fa";
+import { IoIosArrowDown } from "react-icons/io";
 import { ComponentConfig, Fields } from "@measured/puck";
 import * as React from "react";
-import { BasicSelector, Heading, msg, PageSection } from "@yext/visual-editor";
+import {
+  type BackgroundStyle,
+  BasicSelector,
+  Button,
+  Heading,
+  msg,
+  PageSection,
+  useDocument,
+  YextField,
+} from "@yext/visual-editor";
 
-const TEMP_ENDPOINT = "<STREAMS API ENDPOINT GOES HERE>";
+const TEMP_ENDPOINT = "";
 
 export type ReviewsSectionDemoProps = {
-  placeHolder?: string;
+  backgroundColor?: BackgroundStyle;
 };
 
 const reviewsFields: Fields<ReviewsSectionDemoProps> = {
-  placeHolder: BasicSelector({
-    label: "Placeholder",
-    options: [
-      { label: "Some label", value: "some-label" },
-      { label: "Another label", value: "another-label" },
-    ],
-  }),
+  backgroundColor: YextField(
+    msg("fields.backgroundColor", "Background Color"),
+    {
+      type: "select",
+      options: "BACKGROUND_COLOR",
+    },
+  ),
 };
 
 const ReviewsSectionInternal: React.FC<ReviewsSectionDemoProps> = (
   props: ReviewsSectionDemoProps,
 ) => {
-  const { t } = useTranslation();
   const [totalReviews, setTotalReviews] = React.useState(0);
   const [averageRating, setAverageRating] = React.useState(0);
-  React.useEffect(() => {
-    fetchReviews()
-      .then((reviews) => {
-        if (!reviews || !reviews.meta) {
-          throw new Error("Invalid response structure from API");
-        }
-        if (reviews?.meta?.errors && reviews.meta.errors.length > 0) {
-          throw new Error(
-            "API returned errors: " +
-              JSON.stringify(reviews.response.meta.errors),
-          );
-        }
-        setTotalReviews(reviews.response.count || 0);
-        setAverageRating(
-          Number(calculateAverageRating(reviews.response.docs || [])),
+  const [reviewDocs, setReviewDocs] = React.useState<any[]>([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const fetchData = async () => {
+    try {
+      const reviews = await fetchReviews();
+      if (!reviews || !reviews.meta) {
+        throw new Error("Invalid response structure from API");
+      }
+      if (reviews?.meta?.errors && reviews.meta.errors.length > 0) {
+        throw new Error(
+          "API returned errors: " +
+            JSON.stringify(reviews.response.meta.errors),
         );
-      })
-      .catch((error) => {
-        console.error("Error fetching reviews:", error);
-      });
-  }, []);
+      }
+      const reviewsForEntity = filterReviewsToEntity(
+        reviews.response.docs || [],
+      );
+      // TODO: this total/avg logic will change once the data is on the JSON+LD
+      setTotalReviews(reviewsForEntity.length);
+      setAverageRating(Number(calculateAverageRating(reviewsForEntity)));
+      setReviewDocs(reviewsForEntity);
+    } catch (error) {
+      console.error("Error fetching reviews:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  fetchData();
+  const hasDarkBackground = props.backgroundColor?.textColor === "text-white";
   const headerProps: ReviewsHeaderProps = {
     totalReviews,
     averageRating,
+    isLoading,
+    hasDarkBackground,
   };
+  if (!isLoading && totalReviews === 0) {
+    return <></>;
+  }
   return (
-    <PageSection>
-      <div>
+    <PageSection background={props.backgroundColor}>
+      <div className="flex flex-col gap-12">
         <ReviewsHeader {...headerProps} />
+        <ReviewsList
+          reviews={reviewDocs}
+          hasDarkBackground={hasDarkBackground}
+        />
       </div>
     </PageSection>
   );
@@ -63,10 +89,12 @@ const ReviewsSectionInternal: React.FC<ReviewsSectionDemoProps> = (
 interface ReviewsHeaderProps {
   totalReviews: number;
   averageRating: number;
+  isLoading?: boolean;
+  hasDarkBackground: boolean;
 }
 
 const ReviewsHeader: React.FC<ReviewsHeaderProps> = (props) => {
-  const { totalReviews, averageRating } = props;
+  const { totalReviews, averageRating, isLoading, hasDarkBackground } = props;
   const { t } = useTranslation();
   return (
     <div className="flex flex-col gap-3">
@@ -74,16 +102,16 @@ const ReviewsHeader: React.FC<ReviewsHeaderProps> = (props) => {
         {t("recentReviews", "Recent Reviews")}
       </Heading>
       <div className="flex flex-row gap-3 items-center justify-center">
-        {totalReviews === 0 ? (
+        {isLoading ? (
           <div className="text-body-md-fontSize">
-            {t("noReviewsFound", "No reviews available")}
+            {t("loadingReviews", "Loading reviews...")}
           </div>
         ) : (
           <>
-            <div className="font-bold font-body-md-fontWeight text-body-md-fontSize">
-              {averageRating}
-            </div>
-            <ReviewStars rating={averageRating} />
+            <ReviewStarsWithRating
+              rating={averageRating}
+              hasDarkBackground={hasDarkBackground}
+            />
             <div className="text-body-md-fontSize">
               {/* TODO(sumo): update script to handle plurals */}(
               {t("totalReviews", `${totalReviews} reviews`, {
@@ -98,40 +126,269 @@ const ReviewsHeader: React.FC<ReviewsHeaderProps> = (props) => {
   );
 };
 
-const ReviewStars = (props: { rating: number }) => {
-  const { rating } = props;
+const ReviewsList: React.FC<{ reviews: any[]; hasDarkBackground: boolean }> = ({
+  reviews,
+  hasDarkBackground,
+}) => {
+  if (reviews.length === 0) {
+    return <></>; // No reviews to display while loading
+  }
   return (
-    <div className="flex items-center gap-0.5 text-yellow-500">
+    <div className="flex flex-col gap-4">
+      {reviews.map((review, index) => (
+        <Review
+          key={`review-${index}`}
+          review={review}
+          hasDarkBackground={hasDarkBackground}
+        />
+      ))}
+    </div>
+  );
+};
+
+const Review: React.FC<{ review: any; hasDarkBackground: boolean }> = ({
+  review,
+  hasDarkBackground,
+}) => {
+  const authorData: AuthorWithDateProps = {
+    author: review.authorName,
+    date: new Date(review.reviewDate),
+  };
+  const reviewContentData: ReviewContentProps = {
+    rating: review.rating,
+    ...(review.content && { content: review.content }),
+    hasDarkBackground,
+  };
+  const businessResponse =
+    Array.isArray(review.comments) && review.comments.length > 0
+      ? review.comments[0].content
+      : undefined;
+  const businessName = (useDocument() as { name?: string }).name || "";
+  const businessResponseData: BusinessResponseProps = {
+    businessName,
+    content: businessResponse,
+    date: new Date(review.businessResponse?.date || Date.now()),
+  };
+  return (
+    <>
+      <div className="border-t border-gray-400 flex flex-col md:flex-row gap-2 p-4">
+        <div className="w-full md:w-1/3">
+          <AuthorWithDate {...authorData} />
+        </div>
+        <div className="w-full md:w-2/3 flex flex-col gap-8">
+          <ReviewContent {...reviewContentData} />
+          {businessResponse && <BusinessResponse {...businessResponseData} />}
+        </div>
+      </div>
+    </>
+  );
+};
+
+interface AuthorWithDateProps {
+  author: string;
+  date: Date;
+}
+
+const AuthorWithDate: React.FC<AuthorWithDateProps> = ({ author, date }) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="font-bold font-body-fontFamily text-body-lg-fontSize">
+        {author}
+      </div>
+      <div className="font-body-fontFamily text-body-md-fontSize">
+        {formatDate(date)}
+      </div>
+    </div>
+  );
+};
+
+interface ReviewContentProps {
+  rating: number;
+  content?: string;
+  hasDarkBackground: boolean;
+}
+
+const ReviewContent: React.FC<ReviewContentProps> = ({
+  rating,
+  content,
+  hasDarkBackground,
+}) => {
+  const reviewStars = (
+    <ReviewStars rating={rating} hasDarkBackground={hasDarkBackground} />
+  );
+  if (!content) {
+    return <div className="flex flex-col gap-2">{reviewStars}</div>;
+  }
+  const expandableContentData = {
+    content: content,
+    preContentElement: reviewStars,
+  };
+  return <ExpandableContent {...expandableContentData} />;
+};
+
+interface BusinessResponseProps {
+  businessName: string;
+  content: string;
+  date: Date;
+}
+
+const BusinessResponse: React.FC<BusinessResponseProps> = ({
+  businessName,
+  content,
+  date,
+}) => {
+  const { t } = useTranslation();
+  const authorData: AuthorWithDateProps = {
+    author: t("responseFrom", `Response from ${businessName}`, {
+      businessName,
+    }),
+    date: date,
+  };
+  const authorWithDate = <AuthorWithDate {...authorData} />;
+
+  const expandableContentData = {
+    content: content,
+    preContentElement: authorWithDate,
+  };
+  return <ExpandableContent {...expandableContentData} />;
+};
+
+interface ExpandableContentProps {
+  content: string;
+  preContentElement?: React.ReactNode;
+}
+
+const ExpandableContent: React.FC<ExpandableContentProps> = ({
+  content,
+  preContentElement,
+}) => {
+  const [expanded, setExpanded] = React.useState(false);
+  const [isTruncated, setIsTruncated] = React.useState(false);
+  const contentRef = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    if (contentRef.current && !expanded) {
+      // Check if the content is truncated
+      setIsTruncated(
+        contentRef.current.scrollHeight > contentRef.current.clientHeight,
+      );
+    } else {
+      setIsTruncated(false);
+    }
+  }, [content, expanded]);
+
+  return (
+    <div className="flex flex-col gap-2">
+      {preContentElement}
+      <div
+        ref={contentRef}
+        className={`text-body-md-fontSize font-body-fontFamily transition-all duration-200 ${!expanded ? "line-clamp-3" : ""}`}
+        style={{ overflowWrap: "anywhere" }}
+      >
+        {content}
+      </div>
+      {(isTruncated || expanded) && (
+        <ShowMoreButton expanded={expanded} setExpanded={setExpanded} />
+      )}
+    </div>
+  );
+};
+
+interface ReviewStarsProps {
+  rating: number;
+  hasDarkBackground: boolean;
+}
+
+const ReviewStarsWithRating: React.FC<ReviewStarsProps> = ({
+  rating,
+  hasDarkBackground,
+}) => {
+  return (
+    <>
+      <div className="font-bold font-body-md-fontWeight text-body-md-fontSize">
+        {rating}
+      </div>
+      <ReviewStars rating={rating} hasDarkBackground={hasDarkBackground} />
+    </>
+  );
+};
+
+const ReviewStars: React.FC<ReviewStarsProps> = (props) => {
+  const { rating, hasDarkBackground } = props;
+  const HalfStar = hasDarkBackground ? FaStarHalf : FaStarHalfAlt;
+  const starColor = hasDarkBackground
+    ? "text-white"
+    : "text-palette-primary-dark";
+  return (
+    <div className={`flex items-center gap-0.5 ${starColor}`}>
       {new Array(5)
         .fill(null)
         .map((_, i) =>
           rating - i >= 0.75 ? (
             <FaStar key={i} />
           ) : rating - i >= 0.25 ? (
-            <FaStarHalfAlt key={i} />
+            <HalfStar key={i} />
           ) : (
-            <FaRegStar key={i} />
+            <FaRegStar
+              key={i}
+              style={hasDarkBackground ? { display: "none" } : undefined}
+            />
           ),
         )}
     </div>
   );
 };
 
-// function to fetch reviews from the temporary endpoint
-const fetchReviews = async () => {
+const ShowMoreButton: React.FC<{
+  expanded: boolean;
+  setExpanded: (expanded: boolean) => void;
+}> = ({ expanded, setExpanded }) => {
+  const { t } = useTranslation();
+  return (
+    <Button
+      className="font-body-fontFamily text-body-md-fontSize underline cursor-pointer inline-flex items-center"
+      onClick={() => setExpanded(!expanded)}
+      variant={"link"}
+    >
+      {expanded ? t("showLess", "Show less") : t("showMore", "Show more")}
+      <IoIosArrowDown
+        className={`ml-1 transition-transform ${expanded ? "rotate-180" : ""}`}
+        style={{ textDecoration: "underline" }}
+      />
+    </Button>
+  );
+};
+
+/**
+ * Fetch reviews for the business.
+ * @returns A promise that resolves to the reviews data fetched from the endpoint.
+ */
+async function fetchReviews() {
   const response = await fetch(TEMP_ENDPOINT);
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
-  const data = await response.json();
-  return data;
-};
+  return response.json();
+}
+
+/**
+ * Filters the reviews to only include those that belong to the current entity.
+ * @param docs - An array of review documents.
+ * @returns An array of review documents that belong to the current entity.
+ */
+function filterReviewsToEntity(docs: any[]) {
+  // const uid = (useDocument() as { entity?: { uid?: string } }).entity?.uid;
+  const uid = 25897322;
+  return docs.filter((doc) => {
+    return doc.entity?.uid === uid;
+  });
+}
 
 /**
  * Calculates the average rating from an array of review documents, rounding to the nearest tenth place.
  * @param docs - An array of review documents, each containing a rating field.
  */
-const calculateAverageRating = (docs: any[]) => {
+function calculateAverageRating(docs: any[]) {
   let totalRating = 0;
   let count = 0;
   docs.forEach((doc) => {
@@ -141,7 +398,19 @@ const calculateAverageRating = (docs: any[]) => {
     }
   });
   return count > 0 ? (totalRating / count).toFixed(1) : 0;
-};
+}
+
+/**
+ * Returns a formatted date string in the format "Month Day, Year".
+ * @param date - A Date object to format.
+ */
+function formatDate(date: Date): string {
+  return date.toLocaleDateString(undefined, {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
 
 export const ReviewsSectionDemo: ComponentConfig<ReviewsSectionDemoProps> = {
   fields: reviewsFields,
