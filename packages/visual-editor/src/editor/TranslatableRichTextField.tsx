81fe991dd5e3df8bd43b9a34031d0941b1176e75
Alexis Sanehisa <106991746+asanehisa@users.noreply.github.com>
2025-07-01 14:38:55 -0400

    feat: add rtf functionality (#560)
    Adjusted code to work with YSS RTF changes. This should be released
    after YSS is shipped. This also handles multiple locales + having a diff
    RTF static value for each locale.
    
    
    https://github.com/user-attachments/assets/7798edc0-f0db-4b3f-a0b5-9e19767d2dff
    
    ---------
    
    Co-authored-by: coltondemetriou <coltondemetriou@gmail.com>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- packages/visual-editor/src/editor/TranslatableRichTextField.tsx
+++ packages/visual-editor/src/editor/TranslatableRichTextField.tsx
@@ -1,7 +1,7 @@
-import { TranslatableRichText } from "../types/types.ts";
+import { TranslatableRichText, RichText } from "../types/types.ts";
 import { MsgString, pt } from "../utils/i18nPlatform.ts";
 import { CustomField, FieldLabel } from "@measured/puck";
-import { getDisplayValue } from "../utils/resolveTranslatableString.tsx";
+import { resolveTranslatableRichText } from "../utils/resolveTranslatableString.tsx";
 import React from "react";
 import {
   TARGET_ORIGINS,
@@ -22,23 +22,24 @@ export function TranslatableRichTextField<
     render: ({ onChange, value }) => {
       const { i18n } = useTranslation();
       const locale = i18n.language;
-      const resolvedValue = getDisplayValue(value, locale);
-      const fieldLabel = (label && pt(label)) + ` (${locale})`;
+      const resolvedValue = resolveTranslatableRichText(value, locale);
+      const fieldLabel = label ? `${pt(label)} (${locale})` : "";
+
+      const [pendingMessageId, setPendingMessageId] = React.useState<
+        string | undefined
+      >();
 
       const { sendToParent: openConstantValueEditor } = useSendMessageToParent(
         "constantValueEditorOpened",
         TARGET_ORIGINS
       );
 
-      const [pendingMessageId, setPendingMessageId] = React.useState<
-        string | undefined
-      >();
       useReceiveMessage(
         "constantValueEditorClosed",
         TARGET_ORIGINS,
         (_, payload) => {
           if (pendingMessageId && pendingMessageId === payload?.id) {
-            handleNewValue(payload.value);
+            handleNewValue(payload.value, payload.locale);
           }
         }
       );
@@ -46,27 +47,38 @@ export function TranslatableRichTextField<
       const handleClick = () => {
         const messageId = `RichText-${Date.now()}`;
         setPendingMessageId(messageId);
+        const valueForCurrentLocale =
+          typeof value === "object" && value !== null && !Array.isArray(value)
+            ? (value as Record<string, any>)[locale]
+            : undefined;
+
+        const initialValue = React.isValidElement(resolvedValue)
+          ? valueForCurrentLocale?.json
+          : valueForCurrentLocale;
+
         openConstantValueEditor({
           payload: {
-            type: "RichText",
-            value: resolvedValue,
+            type: "RichTextValue", // type: "RichText" is being used in artifact 0.0.8
+            value: initialValue,
             id: messageId,
             fieldName: fieldLabel,
+            locale: locale,
           },
         });
 
-        // localDev
+        /** Handles local development testing outside of storm */
         if (
           window.location.href.includes("http://localhost:5173/dev-location")
         ) {
-          handleNewValue(prompt("Enter text:") ?? "");
+          const userInput = prompt("Enter Rich Text (HTML):");
+          handleNewValue({ json: "", html: userInput ?? "" }, locale);
         }
       };
 
-      const handleNewValue = (newValue: string) => {
+      const handleNewValue = (newValue: RichText, localeToUpdate: string) => {
         onChange({
           ...(typeof value === "object" && !Array.isArray(value) ? value : {}),
-          [locale]: newValue,
+          [localeToUpdate]: newValue,
           hasLocalizedValue: "true",
         } as TranslatableRichText as T);
       };
