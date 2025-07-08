85d13ef178a2af396b0f89cd97a6f84a428cb217
Jacob Wartofsky <jwartofsky@yext.com>
2025-07-08 15:53:54 -0400

    add migration

---
+++ packages/visual-editor/src/components/migrations/0007_add_breadcrumbs_directory_background.ts
@@ -0,0 +1,27 @@
+import { Migration } from "../../utils/migrate.ts";
+import { backgroundColors } from "../../utils/themeConfigOptions.ts";
+
+export const addBreadcrumbsDirectoryBackground: Migration = {
+  // Update Breadcrumbs - add backgroundColor
+  Breadcrumbs: {
+    action: "updated",
+    propTransformation: ({ ...props }) => ({
+      ...props,
+      styles: {
+        backgroundColor: backgroundColors.background1.value,
+      },
+    }),
+  },
+
+  // Update Directory - add backgroundColor and breadcrumbsBackgroundColor
+  Directory: {
+    action: "updated",
+    propTransformation: ({ ...props }) => ({
+      ...props,
+      styles: {
+        backgroundColor: backgroundColors.background1.value,
+        breadcrumbsBackgroundColor: backgroundColors.background1.value,
+      },
+    }),
+  },
+};
