e045025bae8eb3608c577f41461266e7746507d0
Jacob Wartofsky <jwartofsky@yext.com>
2025-07-10 10:40:02 -0400

    Delete BorderRadiusSelector and i18n

--- packages/visual-editor/src/editor/index.ts
+++ packages/visual-editor/src/editor/index.ts
@@ -10,7 +10,6 @@ export {
   type OptionalNumberFieldProps,
 } from "./OptionalNumberField.tsx";
 export { FontSizeSelector } from "./FontSizeSelector.tsx";
-export { BorderRadiusSelector } from "./BorderRadiusSelector.tsx";
 export { SpacingSelector } from "./SpacingSelector.tsx";
 export { BasicSelector } from "./BasicSelector.tsx";
 export { YextField } from "./YextField.tsx";
