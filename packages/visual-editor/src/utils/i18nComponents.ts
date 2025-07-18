09286b5947bebb3ef08335df873600a1e29abbe4
Jacob Wartofsky <jwartofsky@yext.com>
2025-07-18 14:52:27 -0400

    fixed fallbacks

--- packages/visual-editor/src/utils/i18nComponents.ts
+++ packages/visual-editor/src/utils/i18nComponents.ts
@@ -21,12 +21,32 @@ for (const path in modules) {
   }
 }
 
-i18nComponentsInstance.use(initReactI18next).init({
-  fallbackLng: {
-    "zh-Hans": ["zh"],
-    "zh-Hant": ["zh-TW"],
-    default: ["en"],
+type Fallback = {
+  from: string;
+  to: string;
+};
+const fallbacks: Fallback[] = [
+  {
+    from: "zh-Hans",
+    to: "zh",
   },
+  {
+    from: "zh-Hant",
+    to: "zh-TW",
+  },
+];
+fallbacks.forEach(({ from, to }: Fallback) => {
+  if (!!resources[from]) {
+    return;
+  }
+  if (!resources[to]) {
+    return;
+  }
+  resources[from] = resources[to];
+});
+
+i18nComponentsInstance.use(initReactI18next).init({
+  fallbackLng: "en",
   ns: [NAMESPACE],
   defaultNS: NAMESPACE,
   interpolation: { escapeValue: false },
