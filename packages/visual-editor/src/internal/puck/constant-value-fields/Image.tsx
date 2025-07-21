9daaf14a0b560027e93d40c17f47fd2236481903
Alexis Sanehisa <asanehisa@yext.com>
2025-07-21 13:08:46 -0400

    wip

--- packages/visual-editor/src/internal/puck/constant-value-fields/Image.tsx
+++ packages/visual-editor/src/internal/puck/constant-value-fields/Image.tsx
@@ -1,21 +1,60 @@
-import { CustomField } from "@measured/puck";
+import { CustomField, FieldLabel } from "@measured/puck";
 import { ImageType } from "@yext/pages-components";
-import { ConstantFields } from "./ConstantField.tsx";
 import { pt } from "../../../utils/i18nPlatform.ts";
+import * as React from "react";
+import {
+  TARGET_ORIGINS,
+  useReceiveMessage,
+  useSendMessageToParent,
+} from "../../hooks/useMessage";
 
 export const IMAGE_CONSTANT_CONFIG: CustomField<ImageType> = {
   type: "custom",
+  //eslint-disable-next-line @typescript-eslint/no-unused-vars remove this
   render: ({ onChange, value }) => {
-    return ConstantFields({
-      onChange: onChange,
-      value: value,
-      fields: [
-        {
-          label: pt("fields.url", "URL"),
-          field: "url",
-          fieldType: "text",
+    const [pendingMessageId, setPendingMessageId] = React.useState<
+      string | undefined
+    >();
+
+    const { sendToParent: openImageAssetSelector } = useSendMessageToParent(
+      "constantValueEditorOpened",
+      TARGET_ORIGINS
+    );
+
+    useReceiveMessage(
+      "constantValueEditorClosed",
+      TARGET_ORIGINS,
+      (_, payload) => {
+        if (pendingMessageId && pendingMessageId === payload?.id) {
+          console.log(payload);
+          // handle onChange
+        }
+      }
+    );
+
+    const handleClick = () => {
+      const messageId = `ImageAsset-${Date.now()}`;
+      setPendingMessageId(messageId);
+
+      openImageAssetSelector({
+        payload: {
+          type: "ImageAsset",
+          value: value,
+          id: messageId,
         },
-      ],
-    });
+      });
+    };
+
+    // handle local development in /starter
+
+    return (
+      <FieldLabel label={pt("Image")}>
+        <button className="" onClick={handleClick}>
+          <div className="ve-line-clamp-3">
+            {/** Image or "Choose image" Button */}{" "}
+          </div>
+        </button>
+      </FieldLabel>
+    );
   },
 };
