63060b8b4e8d6f2f7c124bf084cf4b95caa59cd9
Jacob Wartofsky <jwartofsky@yext.com>
2025-07-22 16:47:00 -0400

    use config.contentEndpointId. Remove fallbacks

--- packages/visual-editor/src/components/pageSections/NearbyLocations.tsx
+++ packages/visual-editor/src/components/pageSections/NearbyLocations.tsx
@@ -516,9 +516,7 @@ function parseDocument(
   if (document?._pageset) {
     try {
       const pagesetJson = JSON.parse(document?._pageset);
-      contentEndpointId =
-        pagesetJson?.contentEndpointId ??
-        pagesetJson?.typeConfig?.entityConfig?.contentEndpointId;
+      contentEndpointId = pagesetJson?.config?.contentEndpointId;
     } catch (e) {
       console.error("Failed to parse pageset from document. err=", e);
     }
