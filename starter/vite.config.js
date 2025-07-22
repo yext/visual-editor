88ef060301d1f3601745e6f3068e2503c34e2951
coltondemetriou <coltondemetriou@gmail.com>
2025-07-22 15:53:29 -0400

    rm more unnecessary code

--- starter/vite.config.js
+++ starter/vite.config.js
@@ -2,13 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import yextSSG from "@yext/pages/vite-plugin";
 
-// Check if we're running in StackBlitz
-const isStackBlitz = process.env.STACKBLITZ === "true";
-
 export default defineConfig({
   plugins: [react(), yextSSG()],
-  server: {
-    // Disable HMR in StackBlitz to prevent WebSocket connection errors
-    hmr: isStackBlitz ? false : true,
-  },
 });
