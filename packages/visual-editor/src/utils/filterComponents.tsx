d7caa0d0e2ec56d85cddfa086cfdfdde1bc2cbc5
anguyen-yext2 <anguyen@yext.com>
2025-08-06 14:48:52 -0400

    feat: support category filtering
    This PR add support for category filtering
    Slack thread for additional context: https://yext.slack.com/archives/C06A06BCUUF/p1754493867337909
    
    J=WAT-4956
    TEST=manual
    
    reference this packed version in a starter branch and tested in platform.
    1. When the product feature is enabled, the core info category and its atoms appear in the Components column of the Editor. User can drag the grid and subcomponents onto the page.
    2. When the product feature is disabled, the core info category and its atoms are hidden the Components column. Any existing grid sections already placed on the page will show a "No configuration" message in the Editor. These sections will not appear on the live site.
    3. Currently, the atoms under the core info category are exclusive to that category. However, tested scenarios confirm that if an atom is shared across multiple categories:
    - If the user has the atom both standalone and inside a grid, disabling the feature flag only removes the grid and its contents.
    - The standalone atom remains unaffected and continues to render as expected.

--- packages/visual-editor/src/utils/filterComponents.tsx
+++ packages/visual-editor/src/utils/filterComponents.tsx
@@ -1,46 +1,64 @@
 import { DefaultComponentProps, type Config } from "@measured/puck";
 
-// The gated components that are disallowed by default. They are only visible and draggable if a business has the appropriate product features enabled.
-const gatedLayoutComponents: string[] = ["CustomCodeSection", "GridSection"];
+// The components that are disallowed by default. They are only visible and draggable if a business has the appropriate product features enabled.
+const gatedLayoutComponents: string[] = ["CustomCodeSection"];
+
+// The categories that are disallowed by default. Their components are only visible and draggable if a business has the appropriate product features enabled.
+const gatedLayoutCategories: string[] = ["coreInformation"];
 
 /**
  * Filters out gated layout components from a configuration object unless explicitly allowed.
  *
  * @param config - The original configuration object.
  * @param additionalLayoutComponents - An optional list of gated component names to retain.
+ * @param additionalLayoutCategories - An optional list of gated categories to retain.
  * @returns A new configuration object with all gated components removed, except for those listed in `additionalLayoutComponents`.
  */
 export const filterComponentsFromConfig = <T extends DefaultComponentProps>(
   config: Config<T>,
-  additionalLayoutComponents?: string[]
+  additionalLayoutComponents?: string[],
+  additionalLayoutCategories?: string[]
 ): Config<T> => {
-  // Filter components object
-  const filteredComponents = Object.fromEntries(
-    Object.entries(config.components).filter(
-      ([key]) =>
-        !gatedLayoutComponents.includes(key) ||
-        additionalLayoutComponents?.includes(key)
-    )
-  ) as Config<T>["components"];
+  // 1. Filter out gated categories unless they are explicitly allowed
+  const filteredCategories = Object.entries(config.categories || {}).filter(
+    ([categoryKey]) => {
+      const isGated = gatedLayoutCategories.includes(categoryKey);
+      const isGrantedAccess = additionalLayoutCategories?.includes(categoryKey);
+      return !isGated || isGrantedAccess;
+    }
+  );
 
-  // Filter categories by removing gated components that are not present in the additional components list from their components arrays
-  const filteredCategories = Object.fromEntries(
-    Object.entries(config.categories || {}).map(([categoryKey, category]) => [
-      categoryKey,
-      {
-        ...category,
-        components: (category.components || []).filter(
-          (componentName) =>
-            !gatedLayoutComponents.includes(componentName as string) ||
-            additionalLayoutComponents?.includes(componentName as string)
-        ),
-      },
-    ])
+  // 2. Collect all component names under the filtered categories
+  const componentsUnderFilteredCategories = new Set<string>(
+    filteredCategories.flatMap(([, category]) =>
+      (category.components ?? []).map(
+        (componentName) => componentName as string
+      )
+    )
+  );
+
+  // 3. Filter out
+  // - components that are exclusive to the disallowed categories
+  //   (e.g `Grid` component is exclusive to `coreInformation` category. So if `coreInformation` is filtered out, `Grid` will also be filtered out)
+  // - gated components unless they are explicitly allowed
+  const filteredComponents = Object.entries(config.components).filter(
+    ([componentKey]) => {
+      const isUnderFilteredCategories =
+        componentsUnderFilteredCategories.has(componentKey);
+      const isGated = gatedLayoutComponents.includes(componentKey);
+      const isGrantedAccess =
+        additionalLayoutComponents?.includes(componentKey);
+      return (isUnderFilteredCategories && !isGated) || isGrantedAccess;
+    }
   );
 
   return {
     ...config,
-    components: filteredComponents,
-    categories: filteredCategories,
+    components: Object.fromEntries(
+      filteredComponents
+    ) as Config<T>["components"],
+    categories: Object.fromEntries(
+      filteredCategories
+    ) as Config<T>["categories"],
   };
 };
