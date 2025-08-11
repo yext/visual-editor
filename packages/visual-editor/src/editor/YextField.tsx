2fb957e80022226d83c57a01bcd904ffc73410da
Alexis Sanehisa <106991746+asanehisa@users.noreply.github.com>
2025-08-11 10:35:55 -0400

    chore: add TranslatableString type to YextField (#706)
    This is helpful for developing with a unified YextField (especially for
    hybrid developers). Tested in /starter and confirmed translatableStrings
    worked for multiple locales.

--- packages/visual-editor/src/editor/YextField.tsx
+++ packages/visual-editor/src/editor/YextField.tsx
@@ -14,12 +14,15 @@ import {
   CodeField,
   getMaxWidthOptions,
   msg,
+  TranslatableStringField,
 } from "@yext/visual-editor";
 import {
   RenderYextEntityFieldSelectorProps,
   YextEntityField,
   YextEntityFieldSelector,
 } from "./YextEntityFieldSelector.tsx";
+import { RenderEntityFieldFilter } from "../internal/utils/getFilteredEntityFields.ts";
+import { MsgString } from "../utils/i18n/platform.ts";
 
 /** Copied from Puck, do not change */
 export type FieldOption = {
@@ -94,6 +97,12 @@ type YextMaxWidthField = YextBaseField & {
   type: "maxWidth";
 };
 
+type YextTranslatableStringField = YextBaseField & {
+  type: "translatableString";
+  filter?: RenderEntityFieldFilter<any>;
+  showApplyAllOption?: boolean;
+};
+
 // YextEntitySelectorField has same functionality as YextEntityFieldSelector
 type YextEntitySelectorField<
   T extends Record<string, any> = Record<string, any>,
@@ -112,20 +121,21 @@ type YextFieldConfig<Props = any> =
   | YextRadioField
   | YextOptionalNumberField
   | YextCodeField
-  | YextMaxWidthField;
+  | YextMaxWidthField
+  | YextTranslatableStringField;
 
 export function YextField<T = any>(
-  fieldName: string,
+  fieldName: MsgString,
   config: YextFieldConfig<T>
 ): Field<T>;
 
 export function YextField<T extends Record<string, any>, U = any>(
-  fieldName: string,
+  fieldName: MsgString,
   config: YextEntitySelectorField<T>
 ): Field<YextEntityField<U>>;
 
 export function YextField<T, U>(
-  fieldName: string,
+  fieldName: MsgString,
   config: YextFieldConfig<T>
 ): Field<any> {
   // use YextEntityFieldSelector
@@ -207,6 +217,14 @@ export function YextField<T, U>(
         },
       ],
     });
+  }
+
+  if (config.type === "translatableString") {
+    return TranslatableStringField(
+      fieldName,
+      config.filter,
+      config.showApplyAllOption
+    );
   }
 
   return {
