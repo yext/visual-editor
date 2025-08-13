yext/visual-editor

commit cb6ed71d39747b1eae8d5f278d15f07785f32449

refactor: resolve translations and entity fields together (#652)
This adds a new `resolveComponentData` util that resolves both
translations and entity fields for strings and rich text.

---------

Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

packages/visual-editor/src/components/pageSections/HeroSection.tsx
@@ -5,7 +5,6 @@ import { AnalyticsScopeProvider, HoursType } from "@yext/pages-components";
 import {
   HeroSectionType,
   useDocument,
-  resolveYextEntityField,
   EntityField,
   YextEntityField,
   Image,
@@ -18,18 +17,17 @@ import {
   YextField,
   VisibilityWrapper,
   CTAProps,
-  resolveYextStructField,
   YextStructFieldSelector,
   YextStructEntityField,
   ComponentFields,
   HoursStatusAtom,
   TranslatableString,
-  resolveTranslatableString,
   msg,
   pt,
   getAnalyticsScopeHash,
   ReviewStars,
   getAggregateRating,
+  resolveComponentData,
 } from "@yext/visual-editor";
 import {
   ImageStylingFields,
@@ -268,33 +266,23 @@ const heroSectionFields: Fields<HeroSectionProps> = {
 const HeroSectionWrapper = ({ data, styles }: HeroSectionProps) => {
   const { t, i18n } = useTranslation();
   const locale = i18n.language;
-  const streamDocument = useDocument();
-  const resolvedBusinessName = resolveTranslatableString(
-    resolveYextEntityField<TranslatableString>(
-      streamDocument,
-      data?.businessName,
-      locale
-    ),
-    locale
+  const streamDocument = useDocument() as any;
+  const resolvedBusinessName = resolveComponentData(
+    data?.businessName,
+    locale,
+    streamDocument
   );
-  const resolvedLocalGeoModifier = resolveTranslatableString(
-    resolveYextEntityField<TranslatableString>(
-      streamDocument,
-      data?.localGeoModifier,
-      locale
-    ),
-    locale
+  const resolvedLocalGeoModifier = resolveComponentData(
+    data?.localGeoModifier,
+    locale,
+    streamDocument
   );
-  const resolvedHours = resolveYextEntityField<HoursType>(
-    streamDocument,
+  const resolvedHours = resolveComponentData(
     data?.hours,
-    locale
+    locale,
+    streamDocument
   );
-  const resolvedHero = resolveYextStructField(
-    streamDocument,
-    data?.hero,
-    locale
-  );
+  const resolvedHero = resolveComponentData(data?.hero, locale, streamDocument);
 
   const { timezone } = streamDocument as {
     timezone: string;
@@ -382,7 +370,7 @@ const HeroSectionWrapper = ({ data, styles }: HeroSectionProps) => {
                 <CTA
                   eventName={`primaryCta`}
                   variant={styles?.primaryCTA}
-                  label={resolveTranslatableString(
+                  label={resolveComponentData(
                     resolvedHero.primaryCta.label,
                     i18n.language
                   )}
@@ -403,7 +391,7 @@ const HeroSectionWrapper = ({ data, styles }: HeroSectionProps) => {
                 <CTA
                   eventName={`secondaryCta`}
                   variant={styles?.secondaryCTA}
-                  label={resolveTranslatableString(
+                  label={resolveComponentData(
                     resolvedHero.secondaryCta.label,
                     i18n.language
                   )}

packages/visual-editor/src/components/Directory.tsx
@@ -14,10 +14,9 @@ import {
   BackgroundStyle,
   Background,
   HeadingLevel,
-  resolveYextEntityField,
   YextEntityField,
-  resolveTranslatableString,
   getLocationPath,
+  resolveComponentData,
 } from "@yext/visual-editor";
 import { BreadcrumbsComponent } from "./pageSections/Breadcrumbs.tsx";
 import { ComponentConfig, Fields } from "@measured/puck";
@@ -314,14 +313,7 @@ const DirectoryComponent = ({ data, styles }: DirectoryProps) => {
   const { i18n } = useTranslation();
   const { document: streamDocument, relativePrefixToRoot } = useTemplateProps();
 
-  const title = resolveTranslatableString(
-    resolveYextEntityField<TranslatableString>(
-      streamDocument,
-      data.title,
-      i18n.language
-    ),
-    i18n.language
-  );
+  const title = resolveComponentData(data.title, i18n.language, streamDocument);
 
   return (
     <Background background={styles.backgroundColor}>

packages/visual-editor/src/components/contentBlocks/Address.tsx
@@ -9,11 +9,11 @@ import {
 import "@yext/pages-components/style.css";
 import {
   useDocument,
-  resolveYextEntityField,
   EntityField,
   YextEntityField,
   CTA,
   YextField,
+  resolveComponentData,
 } from "@yext/visual-editor";
 
 export type AddressProps = {
@@ -41,10 +41,10 @@ const AddressComponent = ({
 }: AddressProps) => {
   const { t, i18n } = useTranslation();
   const streamDocument = useDocument();
-  const address = resolveYextEntityField(
-    streamDocument,
+  const address = resolveComponentData(
     addressField,
-    i18n.language
+    i18n.language,
+    streamDocument
   );
   const coordinates = getDirections(
     address as AddressType,

packages/visual-editor/src/components/contentBlocks/BodyText.tsx
@@ -5,12 +5,11 @@ import {
   Body,
   BodyProps,
   useDocument,
-  resolveYextEntityField,
+  resolveComponentData,
   EntityField,
   YextEntityField,
   YextField,
   TranslatableString,
-  resolveTranslatableString,
 } from "@yext/visual-editor";
 
 export interface BodyTextProps extends BodyProps {
@@ -42,10 +41,7 @@ const BodyTextComponent = React.forwardRef<HTMLParagraphElement, BodyTextProps>(
         constantValueEnabled={text.constantValueEnabled}
       >
         <Body ref={ref} {...bodyProps}>
-          {resolveTranslatableString(
-            resolveYextEntityField(streamDocument, text, i18n.language),
-            i18n.language
-          )}
+          {resolveComponentData(text, i18n.language, streamDocument)}
         </Body>
       </EntityField>
     );

commit 00033af391eda7455fe101a72eea22c4e2db02ad

fix: heroComponent's entity overrides broke  (#718)
They broke in #652

---------

Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

packages/visual-editor/src/components/pageSections/HeroSection.tsx
@@ -17,6 +17,7 @@ import {
   YextField,
   VisibilityWrapper,
   CTAProps,
+  resolveYextStructField,
   YextStructFieldSelector,
   YextStructEntityField,
   ComponentFields,
@@ -275,7 +276,7 @@ const heroSectionFields: Fields<HeroSectionProps> = {
 const HeroSectionWrapper = ({ data, styles }: HeroSectionProps) => {
   const { t, i18n } = useTranslation();
   const locale = i18n.language;
-  const streamDocument = useDocument() as any;
+  const streamDocument = useDocument();
   const resolvedBusinessName = resolveComponentData(
     data?.businessName,
     locale,
@@ -291,7 +292,11 @@ const HeroSectionWrapper = ({ data, styles }: HeroSectionProps) => {
     locale,
     streamDocument
   );
-  const resolvedHero = resolveComponentData(data?.hero, locale, streamDocument);
+  const resolvedHero = resolveYextStructField(
+    streamDocument,
+    data?.hero,
+    i18n.language
+  );
 
   const { timezone } = streamDocument as {
     timezone: string;