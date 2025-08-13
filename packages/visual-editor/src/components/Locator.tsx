e21f9e7c23326c8a30a57c9b3fa7371aa82f2ae8
Jacob Wartofsky <jwartofsky@yext.com>
2025-08-13 15:07:00 -0400

    styling again

--- packages/visual-editor/src/components/Locator.tsx
+++ packages/visual-editor/src/components/Locator.tsx
@@ -403,7 +403,7 @@ const LocatorInternal = ({
             <Toggle
               pressed={isSelected}
               onPressedChange={(pressed) => handleOpenNowClick(pressed)}
-              className="pxself-start w-auto"
+              className="py-4 px-12 self-start w-auto"
             >
               <span className="inline-flex items-center gap-2">
                 {isSelected ? (
