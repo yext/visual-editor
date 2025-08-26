68b1182ef18cfbe8eb37abd62f9fad68dd5dd73b
Jacob Wartofsky <jwartofsky@yext.com>
2025-08-26 11:28:36 -0400

    re-add getLocationPath test

---
+++ packages/visual-editor/src/utils/getLocationPath.test.ts
@@ -0,0 +1,113 @@
+import { describe, it, expect } from "vitest";
+import { getLocationPath } from "./getLocationPath.ts";
+
+describe("getLocationPath", () => {
+  it("returns slug paths", () => {
+    expect(
+      getLocationPath({ slug: "my-slug", id: "location1" }, "en", "")
+    ).toBe("my-slug");
+
+    expect(
+      getLocationPath({ slug: "my-slug", id: "location1" }, "es", "")
+    ).toBe("my-slug");
+
+    expect(
+      getLocationPath({ slug: "my-slug", id: "location1" }, "en", "../../")
+    ).toBe("../../my-slug");
+  });
+
+  it("returns address-based paths", () => {
+    expect(
+      getLocationPath(
+        {
+          address: {
+            line1: "1101 Wilson Blvd",
+            city: "Arlington",
+            region: "VA",
+            countryCode: "US",
+            postalCode: "22209",
+          },
+          id: "location1",
+          __: {
+            isPrimaryLocale: true,
+          },
+        },
+        "en",
+        ""
+      )
+    ).toBe("va/arlington/1101-wilson-blvd-location1");
+
+    expect(
+      getLocationPath(
+        {
+          address: {
+            line1: "1101 Wilson Blvd",
+            city: "Arlington",
+            region: "VA",
+            countryCode: "US",
+            postalCode: "22209",
+          },
+          id: "location1",
+          __: {
+            isPrimaryLocale: false,
+          },
+        },
+        "es",
+        ""
+      )
+    ).toBe("es/va/arlington/1101-wilson-blvd-location1");
+
+    expect(
+      getLocationPath(
+        {
+          address: {
+            line1: "1101 Wilson Blvd",
+            city: "Arlington",
+            region: "VA",
+            countryCode: "US",
+            postalCode: "22209",
+          },
+          id: "location1",
+        },
+        "en",
+        "../"
+      )
+    ).toBe("../en/va/arlington/1101-wilson-blvd-location1");
+  });
+
+  it("returns id-based paths", () => {
+    expect(
+      getLocationPath(
+        { id: "location1", __: { isPrimaryLocale: true } },
+        "en",
+        ""
+      )
+    ).toBe("location1");
+
+    expect(getLocationPath({ id: "location1" }, "es", "")).toBe("es/location1");
+
+    expect(
+      getLocationPath(
+        { id: "location1", __: { isPrimaryLocale: true } },
+        "en",
+        "../../../"
+      )
+    ).toBe("../../../location1");
+
+    expect(
+      getLocationPath(
+        { id: "location1", __: { isPrimaryLocale: false } },
+        "en",
+        "../../../"
+      )
+    ).toBe("../../../en/location1");
+  });
+
+  it("handles empty values", () => {
+    // @ts-expect-error
+    expect(getLocationPath({}, {}, "")).toBe(undefined);
+
+    // @ts-expect-error
+    expect(getLocationPath(undefined, {}, "")).toBe(undefined);
+  });
+});
