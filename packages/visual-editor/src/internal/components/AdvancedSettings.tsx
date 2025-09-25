530d553deee89a6e8c6df2ef61d95494f00dffce
Brian Stephan <bstephan@yext.com>
2025-09-25 17:26:01 -0400

    moved getSchemaTemplate into util file

--- packages/visual-editor/src/internal/components/AdvancedSettings.tsx
+++ packages/visual-editor/src/internal/components/AdvancedSettings.tsx
@@ -6,6 +6,7 @@ import {
   useReceiveMessage,
   TARGET_ORIGINS,
 } from "../hooks/useMessage";
+import { getSchemaTemplate } from "../../utils/getSchema.ts";
 
 export interface AdvancedSettingsProps {
   /**
@@ -18,86 +19,6 @@ export interface AdvancedSettingsProps {
     schemaMarkup: string;
   };
 }
-
-const LOCAL_BUSINESS_SCHEMA = `{
-  "@context": "https://schema.org",
-  "@type": "LocalBusiness",
-  "name": "[[name]]",
-  "address": {
-    "@type": "PostalAddress",
-    "streetAddress": "[[address.line1]]",
-    "addressLocality": "[[address.city]]",
-    "addressRegion": "[[address.region]]",
-    "postalCode": "[[address.postalCode]]",
-    "addressCountry": "[[address.countryCode]]"
-  },
-  "openingHours": "[[hours]]",
-  "image": "[[photoGallery]]",
-  "description": "[[description]]",
-  "telephone": "[[mainPhone]]",
-  "paymentAccepted": "[[paymentOptions]]",
-  "hasOfferCatalog": "[[services]]"
-}`;
-
-const DIRECTORY_LIST_ITEM_SCHEMA = `{
-  "@type": "ListItem",
-  "position": "[[position]]",
-  "item": {
-    "@type": "Place",
-    "name": "[[name]]",
-    "address": {
-      "@type": "PostalAddress",
-      "streetAddress": "[[address.line1]]",
-      "addressLocality": "[[address.city]]",
-      "addressRegion": "[[address.region]]",
-      "postalCode": "[[address.postalCode]]",
-      "addressCountry": "[[address.countryCode]]"
-    }
-  }
-}`;
-
-// TODO: get produtionDomain/slug to populated urlWriteback
-const FALLBACK_SCHEMA = `{
-  "@context": "https://schema.org",
-  "@type": "Thing",
-  "name": "[[name]]",
-  "description": "[[description]]",
-  "url": "[[urlWriteback]]"
-}`;
-
-// Function to get the appropriate schema template based on entity type
-export const getSchemaTemplate = (entityTypeId?: string): string => {
-  if (!entityTypeId) {
-    return FALLBACK_SCHEMA;
-  }
-
-  if (
-    entityTypeId === "location" ||
-    entityTypeId === "financialProfessional" ||
-    entityTypeId === "healthcareProfessional"
-  ) {
-    return LOCAL_BUSINESS_SCHEMA;
-  } else if (entityTypeId.startsWith("dm_")) {
-    // Determine position based on entity type
-    let position = 1; // default for dm_root
-    if (entityTypeId === "dm_root") {
-      position = 1;
-    } else if (entityTypeId === "dm_country") {
-      position = 2;
-    } else if (entityTypeId === "dm_region") {
-      position = 3;
-    } else if (entityTypeId === "dm_city") {
-      position = 4;
-    }
-
-    return DIRECTORY_LIST_ITEM_SCHEMA.replace(
-      "[[position]]",
-      position.toString()
-    );
-  } else {
-    return FALLBACK_SCHEMA;
-  }
-};
 
 const SCHEMA_MARKUP_FIELD: CustomField<string> = {
   type: "custom",
