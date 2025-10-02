b8c4aa145bf813876eebf3d4e78805485a770e70
briantstephan <70166284+briantstephan@users.noreply.github.com>
2025-10-02 12:18:09 -0400

    feat: provide default schema to parent window (#791)
    When sending the user's schema to the parent to allow for editing, we
    now also send the default schema. A new option in the UI will allow the
    user to restore the default schema and clear any edits they've made.
    
    Ran everything locally and logged the payload that was sent to the
    front-end, confirming that it included the new defaultValue field.
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- packages/visual-editor/src/utils/getSchema.ts
+++ packages/visual-editor/src/utils/getSchema.ts
@@ -32,6 +32,8 @@ const getDefaultSchema = (
   }
 };
 
+const schemaWhitespaceRegex = /\n\s*/g;
+
 const LOCAL_BUSINESS_SCHEMA = `{
   "@context": "https://schema.org",
   "@type": "LocalBusiness",
@@ -50,7 +52,9 @@ const LOCAL_BUSINESS_SCHEMA = `{
   "telephone": "[[mainPhone]]",
   "paymentAccepted": "[[paymentOptions]]",
   "hasOfferCatalog": "[[services]]"
-}`;
+}`
+  .replace(schemaWhitespaceRegex, " ")
+  .trim();
 
 const DIRECTORY_LIST_ITEM_SCHEMA = `{
   "@type": "ListItem",
@@ -67,14 +71,18 @@ const DIRECTORY_LIST_ITEM_SCHEMA = `{
       "addressCountry": "[[address.countryCode]]"
     }
   }
-}`;
+}`
+  .replace(schemaWhitespaceRegex, " ")
+  .trim();
 
 const FALLBACK_SCHEMA = `{
   "@context": "https://schema.org",
   "@type": "Thing",
   "name": "[[name]]",
   "description": "[[description]]",
-}`;
+}`
+  .replace(schemaWhitespaceRegex, " ")
+  .trim();
 
 // Function to get the appropriate schema template based on entity type
 export const getSchemaTemplate = (entityTypeId?: string): string => {
@@ -104,7 +112,9 @@ export const getSchemaTemplate = (entityTypeId?: string): string => {
     return DIRECTORY_LIST_ITEM_SCHEMA.replace(
       "[[position]]",
       position.toString()
-    );
+    )
+      .replace(/\n\s*/g, " ")
+      .trim();
   } else {
     return FALLBACK_SCHEMA;
   }
