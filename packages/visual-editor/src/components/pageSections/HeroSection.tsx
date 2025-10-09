4ed52ae4755dec10c616b5718c75eb03b812c64e
Alexis Sanehisa <asanehisa@yext.com>
2025-10-09 10:18:01 -0400

    re-add w-fit

--- packages/visual-editor/src/components/pageSections/HeroSection.tsx
+++ packages/visual-editor/src/components/pageSections/HeroSection.tsx
@@ -519,7 +519,7 @@ export const HeroSection: ComponentConfig<{ props: HeroSectionProps }> = {
           data,
           "props.slots.ImageSlot[0].props.className",
           themeManagerCn(
-            "w-full h-full",
+            "w-full sm:w-fit h-full",
             data.props.styles.desktopImagePosition === "left"
               ? "mr-auto"
               : "ml-auto"
