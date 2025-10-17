4d76d0f041d0c8496e0dc90fedabbb7efc650d48
Ben Life <blife@yext.com>
2025-10-17 13:35:25 -0400

    fix action bar

--- packages/visual-editor/src/components/pageSections/heroVariants/ClassicHero.tsx
+++ packages/visual-editor/src/components/pageSections/heroVariants/ClassicHero.tsx
@@ -1,4 +1,8 @@
-import { PageSection, themeManagerCn } from "@yext/visual-editor";
+import {
+  PageSection,
+  themeManagerCn,
+  visualEditorMediaQuery,
+} from "@yext/visual-editor";
 import { useTranslation } from "react-i18next";
 import { HeroVariantProps, HeroImageProps } from "../HeroSection";
 import { HeroContent, heroContentParentCn } from "./HeroContent";
@@ -26,6 +30,10 @@ const ClassicHeroImage: PuckComponent<HeroImageProps> = (props) => {
 export const ClassicHero: PuckComponent<HeroVariantProps> = (props) => {
   const { styles, slots, puck, id } = props;
   const { t } = useTranslation();
+  const { sm: isMobile } = visualEditorMediaQuery();
+  const showLeftImage = isMobile
+    ? styles.mobileImagePosition === "top"
+    : styles.desktopImagePosition === "left";
 
   return (
     <PageSection
@@ -34,16 +42,14 @@ export const ClassicHero: PuckComponent<HeroVariantProps> = (props) => {
       className="flex flex-col sm:flex-row gap-6 md:gap-10"
     >
       {/* Desktop left image / Mobile top image */}
-      <ClassicHeroImage
-        id={id + "-image"}
-        className={themeManagerCn(
-          styles.mobileImagePosition === "bottom" && "hidden sm:block",
-          styles.desktopImagePosition === "right" && "sm:hidden"
-        )}
-        styles={styles}
-        slots={slots}
-        puck={puck}
-      />
+      {showLeftImage && (
+        <ClassicHeroImage
+          id={id + "-image"}
+          styles={styles}
+          slots={slots}
+          puck={puck}
+        />
+      )}
       <div
         className={
           heroContentParentCn(styles) + " justify-center lg:min-w-[350px]"
@@ -53,16 +59,14 @@ export const ClassicHero: PuckComponent<HeroVariantProps> = (props) => {
       </div>
 
       {/* Desktop right image / Mobile bottom image */}
-      <ClassicHeroImage
-        id={id + "-image"}
-        className={themeManagerCn(
-          styles.mobileImagePosition === "top" && "hidden sm:block",
-          styles.desktopImagePosition === "left" && "sm:hidden"
-        )}
-        styles={styles}
-        slots={slots}
-        puck={puck}
-      />
+      {!showLeftImage && (
+        <ClassicHeroImage
+          id={id + "-image"}
+          styles={styles}
+          slots={slots}
+          puck={puck}
+        />
+      )}
     </PageSection>
   );
 };
