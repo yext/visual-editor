555a2f042ff0e93fd0938da768b415ef46f8a9c2
Alexis Sanehisa <asanehisa@yext.com>
2025-10-21 10:43:35 -0400

    remove ?

--- packages/visual-editor/src/utils/canonicalUrl.ts
+++ packages/visual-editor/src/utils/canonicalUrl.ts
@@ -1,10 +1,10 @@
 export const getCanonicalUrl = (data: Record<string, any>): string => {
-  let pagePath = data?.path;
+  let pagePath = data.path;
 
   // Same logic that consulting uses to generate canonical URLs
   if (pagePath === "index.html") {
     pagePath = "";
   }
 
-  return `https://${data?.document?.siteDomain}/${pagePath}`;
+  return `https://${data.document.siteDomain}/${pagePath}`;
 };
