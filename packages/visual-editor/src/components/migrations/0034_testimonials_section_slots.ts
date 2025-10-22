d0ea7369bd0dcdd268c1b735ef2fe279fc997eb6
coltondemetriou <coltondemetriou@gmail.com>
2025-10-22 18:15:41 -0400

    fix tests

--- packages/visual-editor/src/components/migrations/0034_testimonials_section_slots.ts
+++ packages/visual-editor/src/components/migrations/0034_testimonials_section_slots.ts
@@ -149,6 +149,8 @@ export const testimonialsSectionSlots: Migration = {
               props: {
                 data: {
                   field: props.data.testimonials.field,
+                  constantValueEnabled:
+                    props.data.testimonials.constantValueEnabled,
                   constantValue: constantValueEnabled
                     ? cards.map((c) => ({ id: c.props.id }))
                     : [],
