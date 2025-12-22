5d215eb5930e060f17d6f416bfb1c160020f0658
Brian Stephan <bstephan@yext.com>
2025-12-22 13:19:19 -0500

    fix type

--- packages/visual-editor/src/components/header/ExpandedHeader.test.tsx
+++ packages/visual-editor/src/components/header/ExpandedHeader.test.tsx
@@ -918,6 +918,7 @@ const tests: ComponentTest[] = [
                   },
                 ],
                 SecondaryCTASlot: [],
+                LinksSlot: [],
               },
             },
           },
