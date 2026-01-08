ed499a4b855228540624a8728407a62bd276751b
Brian Stephan <bstephan@yext.com>
2026-01-08 13:37:24 -0500

    temporarily restore forced error prop

--- packages/visual-editor/src/components/pageSections/PromoSection/ClassicPromo.tsx
+++ packages/visual-editor/src/components/pageSections/PromoSection/ClassicPromo.tsx
@@ -3,9 +3,21 @@ import { PageSection, themeManagerCn } from "@yext/visual-editor";
 import { PromoVariantProps } from "./PromoSection";
 import { PromoMedia } from "./PromoMedia";
 import { PromoContent } from "./PromoContent";
+import { useEffect, useState } from "react";
 
 export const ClassicPromo: PuckComponent<PromoVariantProps> = (props) => {
   const { data, styles, slots } = props;
+  const [shouldThrow, setShouldThrow] = useState(false);
+
+  useEffect(() => {
+    if (data.forceError) {
+      setShouldThrow(true);
+    }
+  }, [data.forceError]);
+
+  if (shouldThrow) {
+    throw new Error("Forced error in ClassicPromo for testing");
+  }
 
   return (
     <PageSection
