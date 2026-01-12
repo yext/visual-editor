98716425e915ee0a99520bfee7af76fa9468eed6
Brian Stephan <bstephan@yext.com>
2026-01-12 12:06:59 -0500

    added screenshots

--- packages/visual-editor/src/components/pageSections/ProfessionalHeroSection.test.tsx
+++ packages/visual-editor/src/components/pageSections/ProfessionalHeroSection.test.tsx
@@ -5,13 +5,14 @@ import {
   ComponentTest,
   transformTests,
 } from "../testing/componentTests.setup.ts";
-import { render as reactRender } from "@testing-library/react";
+import { render as reactRender, waitFor } from "@testing-library/react";
 import {
   ProfessionalHeroSection,
   VisualEditorProvider,
   SlotsCategoryComponents,
 } from "@yext/visual-editor";
 import { Render, Config, resolveAllData } from "@measured/puck";
+import { page } from "@vitest/browser/context";
 
 const defaultProps = ProfessionalHeroSection.defaultProps!;
 
@@ -90,46 +91,54 @@ const tests: ComponentTest[] = [
 ];
 
 describe("ProfessionalHeroSection", () => {
-  transformTests(tests).forEach((test) => {
-    it(test.name, async () => {
-      const config: Config = {
-        components: {
-          ProfessionalHeroSection,
-          ...SlotsCategoryComponents,
+  it.each(transformTests(tests))("$viewport.name $name", async (test) => {
+    const config: Config = {
+      components: {
+        ProfessionalHeroSection,
+        ...SlotsCategoryComponents,
+      },
+    };
+
+    const data = {
+      root: { props: { title: "Test" } },
+      content: [
+        {
+          type: "ProfessionalHeroSection",
+          props: test.props,
         },
-      };
+      ],
+    };
 
-      const data = {
-        root: { props: { title: "Test" } },
-        content: [
-          {
-            type: "ProfessionalHeroSection",
-            props: test.props,
-          },
-        ],
-      };
-
-      const resolvedData = await resolveAllData(data, config, {
-        metadata: {
-          streamDocument: test.document,
-        },
-      });
-
-      const { container } = reactRender(
-        <VisualEditorProvider templateProps={{ document: test.document }}>
-          <Render config={config} data={resolvedData} />
-        </VisualEditorProvider>
-      );
-
-      const results = await axe(container);
-      expect(results).toHaveNoViolations();
-
-      // Basic assertions
-      if (test.props.styles.showImage !== false) {
-        expect(container.querySelector("img")).toBeInTheDocument();
-      }
-
-      expect(container).toBeInTheDocument();
+    const resolvedData = await resolveAllData(data, config, {
+      metadata: {
+        streamDocument: test.document,
+      },
     });
+
+    const { container } = reactRender(
+      <VisualEditorProvider templateProps={{ document: test.document }}>
+        <Render config={config} data={resolvedData} />
+      </VisualEditorProvider>
+    );
+
+    await page.viewport(test.viewport.width, test.viewport.height);
+    const images = Array.from(container.querySelectorAll("img"));
+    await waitFor(() => {
+      expect(images.every((i) => i.complete)).toBe(true);
+    });
+
+    await expect(
+      `ProfessionalHeroSection/[${test.viewport.name}] ${test.name}`
+    ).toMatchScreenshot();
+
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+
+    // Basic assertions
+    if (test.props.styles.showImage !== false) {
+      expect(container.querySelector("img")).toBeInTheDocument();
+    }
+
+    expect(container).toBeInTheDocument();
   });
 });
