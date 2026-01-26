71ea44da9516b918297b668e2f534c2e07aad0d9
briantstephan <70166284+briantstephan@users.noreply.github.com>
2026-01-26 14:16:52 -0500

    feat: add variants and new props to product section (#996)
    This adds new Show/Hide props for each slot, as well as a Variant props
    that allows the user to switch between Immersive, Classic, and Minimal.
    
    Mocks:
    https://www.figma.com/design/sX3bkzkEpQ3g2mJrfSSdmd/Quickstart-Template--2025-?node-id=18773-227889&m=dev
    
    
    
    https://github.com/user-attachments/assets/48baadcc-3c63-4aa5-8b74-15f1fa1ae531
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- packages/visual-editor/src/editor/TranslatableRichTextField.tsx
+++ packages/visual-editor/src/editor/TranslatableRichTextField.tsx
@@ -94,10 +94,22 @@ export function TranslatableRichTextField<
   };
 }
 
+export type GetDefaultRTFOptions = {
+  isBold?: boolean;
+};
+
 /** Returns a simple RTF-format p tag wrapper for text  */
-export const getDefaultRTF = (text: string): RichText => {
+export const getDefaultRTF = (
+  text: string,
+  options?: GetDefaultRTFOptions
+): RichText => {
+  const isBold = options?.isBold ?? false;
+  const format = isBold ? 1 : 0;
+  const fontWeight = isBold ? "700 !important" : "400";
+  const tag = isBold ? "strong" : "span";
+
   return {
-    json: `{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"${text}","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}`,
-    html: `<p dir="ltr" style="font-size: 14.67px; font-weight: 400; line-height: 18.67px; color: rgb(0, 0, 0); margin: 0; padding: 3px 2px 3px 2px; position: relative;"><span>${text}</span></p>`,
+    json: `{"root":{"children":[{"children":[{"detail":0,"format":${format},"mode":"normal","style":"","text":"${text}","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}`,
+    html: `<p dir="ltr" style="font-size: 14.67px; font-weight: ${fontWeight}; line-height: 18.67px; color: rgb(0, 0, 0); margin: 0; padding: 3px 2px 3px 2px; position: relative;"><${tag}>${text}</${tag}></p>`,
   };
 };
