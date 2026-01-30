b4dba5ebd148284ff0b8a9a1758e47068728836f
Duval Kilpatrick <mkilpatrick@yext.com>
2026-01-30 16:46:34 -0500

    updates

--- packages/visual-editor/src/components/testing/componentTests.tailwind.config.ts
+++ packages/visual-editor/src/components/testing/componentTests.tailwind.config.ts
@@ -1,8 +1,8 @@
-// This Tailwind Config applies Theme Editor styles to our components
 import { themeResolver } from "../../utils/themeResolver.ts";
 import { defaultThemeConfig } from "../DefaultThemeConfig.ts";
 import { defaultThemeTailwindExtensions } from "../../utils/themeConfigOptions.ts";
 
+// This Tailwind Config applies Theme Editor styles to our components
 // during playwright testing
 export default {
   content: ["./src/components/**/*.{html,js,jsx,ts,tsx}"],
--- packages/visual-editor/tailwind.config.ts
+++ packages/visual-editor/tailwind.config.ts
@@ -3,7 +3,6 @@ import type { Config } from "tailwindcss";
 export default {
   content: [
     "./src/**/*.{html,js,jsx,ts,tsx}",
-    "!./src/components/puck/registry/**", // exclude the registry
   ],
   prefix: "ve-",
   theme: {
