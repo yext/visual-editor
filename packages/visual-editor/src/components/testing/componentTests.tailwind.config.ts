18aca1f0bd8f2fcd6bda6bfaf51edeaad81d0aa2
Duval Kilpatrick <mkilpatrick@yext.com>
2026-01-30 12:55:32 -0500

    fixes

--- packages/visual-editor/src/components/testing/componentTests.tailwind.config.ts
+++ packages/visual-editor/src/components/testing/componentTests.tailwind.config.ts
@@ -4,12 +4,10 @@ import { defaultThemeConfig } from "../DefaultThemeConfig.ts";
 
 // This Tailwind Config applies Theme Editor styles to our components
 // during playwright testing
-const config = {
+export default {
   content: ["./src/components/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: themeResolver(defaultThemeTailwindExtensions, defaultThemeConfig),
   },
   plugins: [],
 };
-
-export default config;
