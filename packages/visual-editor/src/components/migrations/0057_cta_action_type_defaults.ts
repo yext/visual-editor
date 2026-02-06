4a338b1918d4b821c09da48f00b0f52c70d041fa
Brian Stephan <bstephan@yext.com>
2026-02-06 15:02:37 -0500

    updated migration

--- packages/visual-editor/src/components/migrations/0057_cta_action_type_defaults.ts
+++ packages/visual-editor/src/components/migrations/0057_cta_action_type_defaults.ts
@@ -1,32 +1,43 @@
 import { Migration } from "../../utils/migrate.ts";
+import { StreamDocument } from "../../utils/types/StreamDocument.ts";
+
+const applyCtaDefaults = (
+  props: { id: string } & Record<string, any>,
+  _streamDocument: StreamDocument
+) => {
+  void _streamDocument;
+  const data = props.data ?? {};
+  const buttonText =
+    typeof data.buttonText === "string"
+      ? { en: data.buttonText, hasLocalizedValue: "true" }
+      : data.buttonText;
+  const ariaLabel =
+    typeof data.ariaLabel === "string"
+      ? { en: data.ariaLabel, hasLocalizedValue: "true" }
+      : data.ariaLabel;
+  const updatedData = {
+    ...data,
+    actionType: data.actionType ?? "link",
+    buttonText: buttonText ?? { en: "Button", hasLocalizedValue: "true" },
+    customId: data.customId ?? "",
+    customClass: data.customClass ?? "",
+    dataAttributes: data.dataAttributes ?? [],
+    ariaLabel: ariaLabel ?? { en: "Button", hasLocalizedValue: "true" },
+  };
+
+  return {
+    ...props,
+    data: updatedData,
+  };
+};
 
 export const ctaActionTypeDefaults: Migration = {
   CTAWrapper: {
     action: "updated",
-    propTransformation: (props) => {
-      const data = props.data ?? {};
-      const buttonText =
-        typeof data.buttonText === "string"
-          ? { en: data.buttonText, hasLocalizedValue: "true" }
-          : data.buttonText;
-      const ariaLabel =
-        typeof data.ariaLabel === "string"
-          ? { en: data.ariaLabel, hasLocalizedValue: "true" }
-          : data.ariaLabel;
-      const updatedData = {
-        ...data,
-        actionType: data.actionType ?? "link",
-        buttonText: buttonText ?? { en: "Button", hasLocalizedValue: "true" },
-        customId: data.customId ?? "",
-        customClass: data.customClass ?? "",
-        dataAttributes: data.dataAttributes ?? [],
-        ariaLabel: ariaLabel ?? { en: "Button", hasLocalizedValue: "true" },
-      };
-
-      return {
-        ...props,
-        data: updatedData,
-      };
-    },
+    propTransformation: applyCtaDefaults,
+  },
+  CTASlot: {
+    action: "updated",
+    propTransformation: applyCtaDefaults,
   },
 };
