fdc722e5ae5dfe01d89541c7a9d35e826acbb7d3
rkeerthient <rkeerthi@yext.com>
2026-02-11 18:13:05 +0530

    added native styling

--- packages/visual-editor/src/components/pageSections/SearchSection/Cards.tsx
+++ packages/visual-editor/src/components/pageSections/SearchSection/Cards.tsx
@@ -1,17 +1,25 @@
 import { CardProps, DefaultRawDataType } from "@yext/search-ui-react";
 import { CardTypeProp } from "./propsAndTypes.ts";
+import { Heading } from "../../atoms/heading.tsx";
+import { Body } from "../../atoms/body.tsx";
 
 interface CardsProps extends CardProps<DefaultRawDataType> {
   cardType?: CardTypeProp;
 }
 
 const Cards = ({ result, cardType = "Standard" }: CardsProps) => {
-  const name =
-    typeof result.rawData?.name === "string" ? result.rawData.name : "";
+  // const name =
+  //   typeof result.rawData?.name === "string" ? result.rawData.name : "";
 
   return (
-    <div>
-      {cardType} — {name}
+    <div className="flex flex-col rounded-lg overflow-hidden border bg-white h-full">
+      <div className="grow"></div>
+      <div className="p-8">
+        <div className="flex flex-col gap-1">
+          <Heading level={3}>{(result.rawData?.name as any) ?? "name"}</Heading>
+          <Body>{cardType}</Body>
+        </div>
+      </div>
     </div>
   );
 };
