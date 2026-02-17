bd2373d1f79fbac11716ddd443d382bb58f00b8e
rkeerthient <rkeerthi@yext.com>
2026-02-17 22:55:28 +0530

    Fixed map in universal

--- packages/visual-editor/src/components/pageSections/SearchSection/LayoutSections.tsx
+++ packages/visual-editor/src/components/pageSections/SearchSection/LayoutSections.tsx
@@ -1,4 +1,7 @@
+import { Coordinate, Map, MapboxMaps, Marker } from "@yext/pages-components";
 import { DefaultRawDataType, SectionProps } from "@yext/search-ui-react";
+import { MapPin } from "lucide-react";
+import "mapbox-gl/dist/mapbox-gl.css";
 import { VerticalLayout } from "./propsAndTypes.ts";
 
 interface LayoutSectionProps extends SectionProps<DefaultRawDataType> {
@@ -23,16 +26,53 @@ export const LayoutSection = ({
 
   // const filteredResults = results.slice(0, resultsCount);
 
+  const coordinates: Coordinate[] = results
+    .map((result) => result.rawData?.yextDisplayCoordinate)
+    .filter((coord): coord is Coordinate => Boolean(coord));
+
+  const coordinateResults = results.filter(
+    (r) => r.rawData?.yextDisplayCoordinate
+  );
+  const firstCoord: any =
+    coordinateResults[0]?.rawData?.yextDisplayCoordinate ?? undefined;
+
   return (
     <div className="flex flex-col mt-12">
       <div className="px-5 py-2.5 flex items-end border rounded-t-md">
         <h2 className="text-[22px]">{header?.props.label}</h2>
       </div>
-      {/* {layoutType === "Map" && results.length > 0 && (
-        <div id="locatorMapDiv" className="w-full h-[300px] border-x border-t">
-          <Map />
+      {layoutType === "Map" && coordinateResults.length > 0 && firstCoord && (
+        <div className="w-full h-[300px] border-x border-t">
+          <Map
+            apiKey={""}
+            controls={false}
+            provider={MapboxMaps}
+            bounds={coordinates}
+            iframeId="preview-frame"
+            singleZoom={undefined}
+            providerOptions={{
+              maxZoom: 12,
+            }}
+            className="w-full h-[300px]"
+          >
+            {results.map((result, index) => {
+              const coord: any = result.rawData?.yextDisplayCoordinate;
+              if (!coord) return null;
+
+              return (
+                <Marker
+                  key={result.id ?? index}
+                  coordinate={coord}
+                  id={index + ""}
+                >
+                  <MapPin size={24} fill="#ef4444" color="#ef4444" />
+                </Marker>
+              );
+            })}
+          </Map>
         </div>
-      )} */}
+      )}
+
       <div className={`${layoutClasses} w-full border rounded-b-md divide-y`}>
         {results.map((result, index) => (
           <CardComponent key={index} result={result} />
