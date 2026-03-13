aae5e9c7f1e973dc8d1d7d8744890e3a16bc66f5
Jacob Wartofsky <jwartofsky@yext.com>
2026-03-13 16:01:42 -0400

    reset ve.config.tsx to component-gen

--- starter/src/ve.config.tsx
+++ starter/src/ve.config.tsx
@@ -8,31 +8,13 @@ import {
   MainConfigProps,
   mainConfig,
 } from "@yext/visual-editor";
-import { SweetgreenFaqSection } from "./registry/sweetgreen/components/SweetgreenFaqSection";
-import { SweetgreenFindUsSection } from "./registry/sweetgreen/components/SweetgreenFindUsSection";
-import { SweetgreenFooterSection } from "./registry/sweetgreen/components/SweetgreenFooterSection";
-import { SweetgreenLocationHeroSection } from "./registry/sweetgreen/components/SweetgreenLocationHeroSection";
-import { SweetgreenMenuSection } from "./registry/sweetgreen/components/SweetgreenMenuSection";
-import { SweetgreenPromoCardsSection } from "./registry/sweetgreen/components/SweetgreenPromoCardsSection";
-import { SweetgreenRequestLocationSection } from "./registry/sweetgreen/components/SweetgreenRequestLocationSection";
-import { SweetgreenSiteHeaderSection } from "./registry/sweetgreen/components/SweetgreenSiteHeaderSection";
-import { SweetgreenSustainabilitySection } from "./registry/sweetgreen/components/SweetgreenSustainabilitySection";
 
 interface DevProps extends MainConfigProps, DirectoryCategoryProps {}
 
-const components = {
+const components: Config<DevProps>["components"] = {
   ...mainConfig.components,
   ...DirectoryCategoryComponents,
-  SweetgreenSiteHeaderSection,
-  SweetgreenLocationHeroSection,
-  SweetgreenFindUsSection,
-  SweetgreenMenuSection,
-  SweetgreenPromoCardsSection,
-  SweetgreenRequestLocationSection,
-  SweetgreenSustainabilitySection,
-  SweetgreenFaqSection,
-  SweetgreenFooterSection,
-} as Config<DevProps>["components"];
+};
 
 export const devConfig: Config<DevProps> = {
   components,
