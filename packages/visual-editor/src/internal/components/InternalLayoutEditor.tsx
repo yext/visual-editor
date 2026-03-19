e9a0275d714a802962d607175b384ccc7038bf74
briantstephan <70166284+briantstephan@users.noreply.github.com>
2026-03-19 15:15:16 -0400

    fix: clone data before resolving in editor (#1084)
    This fixes an issue with team section and some other components where
    entity fields in the editor do not get updated when switching entities.
    
    Confirmed on a test account that switching between entities updates
    fields in the editor as expected.
    https://www.yext.com/s/4520471/yextsites/164673/pagesets

--- packages/visual-editor/src/internal/components/InternalLayoutEditor.tsx
+++ packages/visual-editor/src/internal/components/InternalLayoutEditor.tsx
@@ -38,6 +38,7 @@ import { useDocument } from "../../hooks/useDocument.tsx";
 import { fieldsOverride } from "../puck/components/FieldsOverride.tsx";
 import { isDeepEqual } from "../../utils/deepEqual.ts";
 import { useErrorContext } from "../../contexts/ErrorContext.tsx";
+import { clonePuckResolveData } from "../utils/clonePuckResolveData.ts";
 
 const devLogger = new DevLogger();
 const usePuck = createUsePuck();
@@ -279,15 +280,17 @@ export const InternalLayoutEditor = ({
         const resolveData = async () => {
           devLogger.logFunc("reloadDataOnDocumentChange");
           const { appState, config, dispatch } = getPuck();
-          const dataSnapshot = structuredClone(appState.data);
 
-          const resolvedData = await resolveAllData(appState.data, config, {
+          // Clone Puck data to ensure entity fields get updated on entity selection
+          const dataToResolve = clonePuckResolveData(appState.data);
+
+          const resolvedData = await resolveAllData(dataToResolve, config, {
             streamDocument,
           });
 
           devLogger.logData("RESOLVED_LAYOUT_DATA", resolvedData);
 
-          if (isDeepEqual(dataSnapshot, resolvedData)) {
+          if (isDeepEqual(appState.data, resolvedData)) {
             devLogger.log(
               "reloadDataOnDocumentChange - no layout changes detected"
             );
