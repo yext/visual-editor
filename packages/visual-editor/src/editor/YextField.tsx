a260d1149797651775d6fcf61dd34ea49aab1347
Duval Kilpatrick <mkilpatrick@yext.com>
2026-04-08 17:40:56 -0400

    pass visible

--- packages/visual-editor/src/editor/YextField.tsx
+++ packages/visual-editor/src/editor/YextField.tsx
@@ -263,6 +263,7 @@ export function YextField<T, U>(
     return {
       type: "code",
       label: fieldName,
+      visible: config.visible,
       codeLanguage: config.codeLanguage,
     };
   }
