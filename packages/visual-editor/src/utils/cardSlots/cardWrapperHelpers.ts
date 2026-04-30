3aceeacd44641abe3f00462ac6625848d35e10cd
Jacob Wartofsky <jwartofsky@yext.com>
2026-04-30 17:42:52 -0400

    fix crash

--- packages/visual-editor/src/utils/cardSlots/cardWrapperHelpers.ts
+++ packages/visual-editor/src/utils/cardSlots/cardWrapperHelpers.ts
@@ -1,5 +1,6 @@
 import { Fields, Slot } from "@puckeditor/core";
 import { YextEntityField } from "../../editor/YextEntityFieldSelector.tsx";
+import { YextField } from "../../editor/YextField.tsx";
 import { MsgString } from "../i18n/platform.ts";
 import { EntityFieldTypes } from "../../internal/utils/getFilteredEntityFields.ts";
 import { SourceRootKind } from "./mappedSource.ts";
@@ -26,8 +27,7 @@ export const cardWrapperFields = <T>(
   sourceRootsOnly = false,
   requiredDescendantTypes?: EntityFieldTypes[][]
 ): Fields<CardWrapperType<T>> => ({
-  data: {
-    label,
+  data: YextField(label, {
     type: "entityField",
     filter: {
       listFieldName,
@@ -36,7 +36,7 @@ export const cardWrapperFields = <T>(
       sourceRootKinds,
       sourceRootsOnly,
     },
-  } as any,
+  }),
   slots: {
     type: "object",
     objectFields: {
