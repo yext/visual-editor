e15032deca2aa790d7d1daa3920f45c3b55e5975
Jacob Wartofsky <jwartofsky@yext.com>
2026-05-07 14:06:25 -0400

    revert ColorSelector to main

--- packages/visual-editor/src/fields/ColorSelector.tsx
+++ packages/visual-editor/src/fields/ColorSelector.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { CustomField, FieldLabel } from "@puckeditor/core";
-import reactColor from "react-color";
-import type { Color, ColorResult } from "react-color";
+import { Color, ColorResult, SketchPicker } from "react-color";
 import { pt } from "../utils/i18n/platform.ts";
 
 type RenderProps = Parameters<CustomField<any>["render"]>[0];
@@ -81,7 +80,7 @@ export const ColorPickerInput = ({
             style={fieldStyles.cover}
             onClick={() => setIsOpen(false)}
           />
-          <reactColor.SketchPicker
+          <SketchPicker
             disableAlpha={true}
             color={draftColor}
             onChange={handlePickerChange}
