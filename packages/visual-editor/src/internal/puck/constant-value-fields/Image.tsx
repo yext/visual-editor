c0c5653a168b325ed24f96a43de3b01aef00d878
Jacob Wartofsky <jwartofsky@yext.com>
2026-05-07 18:34:21 -0400

    restore IMAGE_LIST_CONSTANT_CONFIG details

--- packages/visual-editor/src/internal/puck/constant-value-fields/Image.tsx
+++ packages/visual-editor/src/internal/puck/constant-value-fields/Image.tsx
@@ -1,6 +1,7 @@
 import { type YextFieldDefinition } from "../../../fields/fields.ts";
 import { type ImageField } from "../../../fields/ImageField.tsx";
 import { pt } from "../../../utils/i18n/platform.ts";
+import { getRandomPlaceholderImageObject } from "../../../utils/imagePlaceholders.ts";
 
 export const IMAGE_CONSTANT_CONFIG: ImageField = {
   type: "image",
@@ -15,4 +16,12 @@ export const IMAGE_LIST_CONSTANT_CONFIG: YextFieldDefinition = {
       label: pt("fields.image", "Image"),
     },
   },
+  defaultItemProps: {
+    assetImage: {
+      ...getRandomPlaceholderImageObject({ width: 1000, height: 570 }),
+      width: 1000,
+      height: 570,
+    },
+  },
+  getItemSummary: (_, i) => pt("photo", "Photo") + " " + ((i ?? 0) + 1),
 };
