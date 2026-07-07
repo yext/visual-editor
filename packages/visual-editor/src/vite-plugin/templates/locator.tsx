c0201cbdda428d4629d04bbeba25a96f801abd9e
Ben Life <blife@yext.com>
2026-07-07 11:58:02 -0400

    fix favicons

--- packages/visual-editor/src/vite-plugin/templates/locator.tsx
+++ packages/visual-editor/src/vite-plugin/templates/locator.tsx
@@ -45,13 +45,6 @@ export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = (
     charset: "UTF-8",
     viewport: "width=device-width, initial-scale=1",
     tags: [
-      {
-        type: "link" as TagType,
-        attributes: {
-          rel: "icon",
-          type: "image/x-icon",
-        },
-      },
       ...(data.document.siteDomain
         ? [
             {
@@ -80,7 +73,7 @@ export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = (
               type: "link" as TagType,
               attributes: {
                 rel: "icon",
-                type: "image/x-icon",
+                type: "image/png",
                 href: faviconUrl,
               },
             },
