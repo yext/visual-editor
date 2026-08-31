38e5aa4b5eeb9516140696d43ab0d20ad96ae34e
Aaron Friedman <afriedman@yext.com>
2026-08-31 09:19:00 -0400

    WIP: fix spacing for blank links in header

---
+++ packages/visual-editor/src/utils/resolveLocalizedCtas.ts
@@ -0,0 +1,21 @@
+import { type TranslatableCTA } from "../types/types.ts";
+import { resolveComponentData } from "./resolveComponentData.tsx";
+
+export type ResolvedCTA = Omit<TranslatableCTA, "label" | "link"> & {
+  label: string;
+  link: string;
+};
+
+export const resolveLocalizedCtas = (
+  ctas: TranslatableCTA[] | undefined,
+  locale: string,
+  streamDocument?: Record<string, any>
+): ResolvedCTA[] => {
+  return (ctas ?? [])
+    .map((cta) => ({
+      ...cta,
+      label: resolveComponentData(cta.label, locale, streamDocument),
+      link: resolveComponentData(cta.link, locale, streamDocument),
+    }))
+    .filter((cta) => cta.label.trim() !== "" && cta.link.trim() !== "");
+};
